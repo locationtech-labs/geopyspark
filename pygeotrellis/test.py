e2e8b6eda9efd772559b18dfe87dbd2f7819cb9b
jbouffard <jbouffard@azavea.com>
2016-12-15 15:18:42 -0500

    Continuing to Work on the python
    Signed-off-by: jbouffard <jbouffard@azavea.com>

--- pygeotrellis/test.py
+++ pygeotrellis/test.py
@@ -11,10 +11,9 @@ if __name__ == "__main__":
     crs = hssgr.fromEpsgCode(3355)
 
     original_splits = hssgr.split(258, 258)
-    #print ' THESE ARE THE SPLITS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1'
-    #print original_splits
+    print 'THESE ARE THE SPLITS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1'
+    print original_splits
 
-    sc._gateway.jvm.original_splits.map(lambda x: x)
     reprojected_splits = hssgr.reproject(original_splits, crs)
 
     original_value = original_splits.first()
