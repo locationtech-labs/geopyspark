42bdc33532e17561038deff6d8d5694587fae422
jbouffard <jbouffard@azavea.com>
2016-12-15 09:54:18 -0500

    Removed uneeded files
    Signed-off-by: jbouffard <jbouffard@azavea.com>

--- pygeotrellis/test_elt.py
+++
@@ -1,8 +0,0 @@
-from py4j.java_gateway import java_import
-from pyspark import SparkContext
-
-if __name__ == "__main__":
-
-    sc = SparkContext(appName="python-etl-test")
-    java_import(sc._gateway.jvm, "geotrellis.spark-etl.*")
-    java_import(sc._gateway.jvm, "geotrellis.spark-etl.hadoop.*")
