2375bd77ef91d05e3b5a94aa5881ed4f67669376
jbouffard <jbouffard@azavea.com>
2016-12-21 12:20:48 -0500

    Continued work on Avro serialization
    Signed-off-by: jbouffard <jbouffard@azavea.com>

--- geopyspark/test.py
+++ geopyspark/test.py
@@ -7,43 +7,6 @@ import io
 import zlib
 import struct
 import sys
-import avro
-import avro.io
-import binascii
-import StringIO
-
-
-class AvroSerializer(FramedSerializer):
-
-    def __init__(self, schema):
-        self.schema = avro.schema.parse(schema)
-        self.reader = avro.io.DatumReader(self.schema)
-        self.datum_writer = avro.io.DatumWriter(self.schema)
-
-    """
-    Deserializes a byte array into an object.
-    """
-    def dumps(self, obj):
-        writer = StringIO.StringIO()
-        encoder = avro.io.BinaryEncoder(writer)
-        datum = {
-            'col': obj[0].col,
-            'row': obj[0].row
-        }
-        self.datum_writer.write(datum, encoder)
-        return writer.getvalue()
-
-
-    """
-    Serialize an object into a byte array.
-    When batching is used, this will be called with an array of objects.
-    """
-
-    def loads(self, obj):
-        buf = io.BytesIO(obj)
-        decoder = avro.io.BinaryDecoder(buf)
-        i = self.reader.read(decoder)
-        return [SpatialKey(i.get('col'), i.get('row'))]
 
 
 if __name__ == "__main__":
