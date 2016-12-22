335e5962a97a9be803ba564344a914078c0bbac5
jbouffard <jbouffard@azavea.com>
2016-12-22 10:21:59 -0500

    Cleaned up the code in the test script
    Signed-off-by: jbouffard <jbouffard@azavea.com>

--- geopyspark/test.py
+++ geopyspark/test.py
@@ -1,7 +1,8 @@
 from pyspark import SparkConf, SparkContext, RDD
 from pyspark.serializers import Serializer, FramedSerializer, AutoBatchedSerializer
 from py4j.java_gateway import java_import
-from spatial_key import SpatialKey
+from geopyspark.spatial_key import SpatialKey
+from geopyspark.avroserializer import AvroSerializer
 
 import io
 import zlib
@@ -10,21 +11,23 @@ import sys
 
 
 if __name__ == "__main__":
-    schema = """{"type":"record","name":"SpatialKey","namespace":"geotrellis.spark","fields":[{"name":"col","type":"int"},{"name":"row","type":"int"}]}"""
-    ser = AvroSerializer(schema)
     sc = SparkContext(master="local", appName="python-test")
 
     java_import(sc._gateway.jvm, "geopyspark.geotrellis.Box")
+    java_import(sc._gateway.jvm, "geopyspark.geotrellis.ExtentWrapper")
 
-    testRdd = sc._gateway.jvm.Box.testRdd(sc._jsc.sc())
-    encodedRdd = sc._gateway.jvm.Box.encodeRdd(testRdd)
-    javaRdd = encodedRdd.toJavaRDD()
+    box_test_rdd = sc._gateway.jvm.Box.testRdd(sc._jsc.sc())
+    box_encoded_rdd = sc._gateway.jvm.Box.encodeRdd(box_test_rdd)
+    box_java_rdd = box_encoded_rdd.toJavaRDD()
 
-    pythonRdd = RDD(javaRdd, sc, AutoBatchedSerializer(ser))
+    box_schema = sc._gateway.jvm.Box.keySchema()
+    box_ser = AvroSerializer(box_schema)
 
-    thing = pythonRdd.collect()
+    box_python_rdd = RDD(box_java_rdd, sc, AutoBatchedSerializer(box_ser))
+
+    thing = box_python_rdd.collect()
     print "THIS IS THE ITEM HELD WITHIN THE PYTHON RDD:", thing
 
-    rdd2 = pythonRdd.map(lambda s: SpatialKey(s.col+1, s.row+1))
+    rdd2 = box_python_rdd.map(lambda s: SpatialKey(s.col+1, s.row+1))
     result = rdd2.collect()
     print "THIS IS THE RESULT OF THE PYTHON RDD BEING MAPPED OVER:", result
