f99dfb4e6d946afaa0b66574f567f1552665f1cf
jbouffard <jbouffard@azavea.com>
2016-12-22 13:50:03 -0500

    Updated the versions of Scala and GeoTrellis
    Signed-off-by: jbouffard <jbouffard@azavea.com>

--- geopyspark-backend/project/Version.scala
+++ geopyspark-backend/project/Version.scala
@@ -1,7 +1,7 @@
 object Version {
   val geopyspark = "0.0.1"
-  val geotrellis = "1.0.0-RC3"
-  val scala       = "2.10.4"
+  val geotrellis = "1.0.0"
+  val scala       = "2.11.8"
   val crossScala  = Seq("2.11.8", "2.10.6")
   val scalaTest   = "2.2.0"
 }
