a039980eed5f8984669187ee5a56ed252cb7255b
jbouffard <jbouffard@azavea.com>
2017-01-05 13:45:51 -0500

    Created ShortArrayTileWrapper
    Signed-off-by: jbouffard <jbouffard@azavea.com>

---
+++ geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/ShortArrayTileWrapper.scala
@@ -0,0 +1,24 @@
+package geopyspark.geotrellis
+
+import geotrellis.raster._
+import geotrellis.spark._
+import geotrellis.spark.io._
+
+import org.apache.spark._
+import org.apache.spark.rdd.RDD
+
+object ShortArrayTileWrapper extends Wrapper[ShortArrayTile] {
+
+  def testRdd(sc: SparkContext): RDD[ShortArrayTile] = {
+    val arr = Array(
+      ShortArrayTile(Array[Byte](0, 0, 1, 1), 2, 2),
+      ShortArrayTile(Array[Byte](1, 2, 3, 4), 2, 2),
+      ShortArrayTile(Array[Byte](5, 6, 7, 8), 2, 2))
+    println("\n\n\n")
+    println("THESE ARE THE ORIGINAL SHORTARRAYTILES")
+    arr.foreach(println)
+    println("\n\n\n")
+    sc.parallelize(arr)
+  }
+}
+
