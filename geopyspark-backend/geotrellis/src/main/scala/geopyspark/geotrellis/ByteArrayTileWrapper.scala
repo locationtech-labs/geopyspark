387c5aefc7d5152f42b5c6ab5eeb066e13eec833
jbouffard <jbouffard@azavea.com>
2017-01-05 13:44:36 -0500

    Cleaned up ByteArrayTileWrapper
    Signed-off-by: jbouffard <jbouffard@azavea.com>

--- geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/ByteArrayTileWrapper.scala
+++ geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/ByteArrayTileWrapper.scala
@@ -3,22 +3,11 @@ package geopyspark.geotrellis
 import geotrellis.raster._
 import geotrellis.spark._
 import geotrellis.spark.io._
-import geotrellis.spark.io.avro._
-import geotrellis.spark.util.KryoWrapper
 
 import org.apache.spark._
 import org.apache.spark.rdd.RDD
-import org.apache.spark.api.java.JavaRDD
-import org.apache.avro._
 
 object ByteArrayTileWrapper extends Wrapper[ByteArrayTile] {
-
-  override def testIn(rdd: RDD[Array[Byte]], schema: String) = {
-    val result = PythonTranslator.fromPython[ByteArrayTile](rdd, Some(schema))
-    val collection = result.collect()
-    println("\n\n\nTHESE ARE THE NEW ARRAYS")
-    collection.map(x => println(x.array.mkString(" ")))
-  }
 
   def testRdd(sc: SparkContext): RDD[ByteArrayTile] = {
     val arr = Array(
