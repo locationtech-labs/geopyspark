f346c7548b5a81616701469e8efbffcf930db3db
jbouffard <jbouffard@azavea.com>
2017-01-05 13:46:49 -0500

    Created DoubleArrayTileWrapper
    Signed-off-by: jbouffard <jbouffard@azavea.com>

---
+++ geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/DoubleArrayTileWrapper.scala
@@ -0,0 +1,24 @@
+package geopyspark.geotrellis
+
+import geotrellis.raster._
+import geotrellis.spark._
+import geotrellis.spark.io._
+
+import org.apache.spark._
+import org.apache.spark.rdd.RDD
+
+object DoubleArrayTileWrapper extends Wrapper[DoubleArrayTile] {
+
+  def testRdd(sc: SparkContext): RDD[DoubleArrayTile] = {
+    val arr = Array(
+      DoubleArrayTile(Array[Byte](0, 0, 1, 1), 2, 2),
+      DoubleArrayTile(Array[Byte](1, 2, 3, 4), 2, 2),
+      DoubleArrayTile(Array[Byte](5, 6, 7, 8), 2, 2))
+    println("\n\n\n")
+    println("THESE ARE THE ORIGINAL DOUBLEARRAYTILES")
+    arr.foreach(println)
+    println("\n\n\n")
+    sc.parallelize(arr)
+  }
+}
+
