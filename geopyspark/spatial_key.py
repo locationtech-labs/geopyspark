55237b966da18dfd86c56e4966f944da5ee70f77
jbouffard <jbouffard@azavea.com>
2017-01-06 16:02:42 -0500

    Changed spatil_key to keys so that it contains both SpatialKey and SpaceTimeKey
    Signed-off-by: jbouffard <jbouffard@azavea.com>

--- geopyspark/spatial_key.py
+++
@@ -1,7 +0,0 @@
-class SpatialKey(object):
-    def __init__(self, col, row):
-        self.col = col
-        self.row = row
-
-    def __repr__(self):
-        return "SpatialKey(%d,%d)" % (self.col, self.row)
