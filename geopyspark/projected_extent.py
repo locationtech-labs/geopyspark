6b4d0a7ad39dc7f7b29be3b0242655800913e77d
jbouffard <jbouffard@azavea.com>
2017-01-23 18:15:12 -0500

    Added class methods to test for equality

--- geopyspark/projected_extent.py
+++ geopyspark/projected_extent.py
@@ -3,6 +3,16 @@ class ProjectedExtent(object):
         self.extent = extent
         self.epsg_code = epsg_code
 
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, self.__class__):
+            return not self.__eq__(other)
+        return NotImplemented
+
     def __repr__(self):
         return 'ProjectedExtent(Extent: {}, EPSG: {})'.format(
                 self.extent,
--- geopyspark/temporal_projected_extent.py
+++ geopyspark/temporal_projected_extent.py
@@ -4,6 +4,16 @@ class TemporalProjectedExtent(object):
         self.epsg_code = epsg_code
         self.instant = instant
 
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, self.__class__):
+            return not self.__eq__(other)
+        return NotImplemented
+
     def __repr__(self):
         return 'TemporalProjectedExtent(Extemt: {}, EPSG: {}, Instant: {})'.format(
                 self.extent,
