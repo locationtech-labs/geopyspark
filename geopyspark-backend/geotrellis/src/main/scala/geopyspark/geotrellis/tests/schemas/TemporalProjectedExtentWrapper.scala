b74b6fd462c6f21c1980af95e9a3a197ab41ab5a
jbouffard <jbouffard@azavea.com>
2017-01-23 18:16:25 -0500

    Moved ProjectedExtent and TemporalProjectedExtent to tests.schemas in the geotrellis sub-project

---
+++ geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/tests/schemas/TemporalProjectedExtentWrapper.scala
@@ -0,0 +1,22 @@
+package geopyspark.geotrellis.tests.schemas
+
+import geopyspark.geotrellis.testkit._
+
+import geotrellis.proj4._
+import geotrellis.vector.Extent
+import geotrellis.spark._
+import geotrellis.spark.io._
+
+import org.apache.spark._
+import org.apache.spark.rdd.RDD
+
+object TemporalProjectedExtentWrapper extends Wrapper[TemporalProjectedExtent]{
+
+  def testRdd(sc: SparkContext): RDD[TemporalProjectedExtent] = {
+    val arr = Array(
+      TemporalProjectedExtent(Extent(0, 0, 1, 1), CRS.fromEpsgCode(2004), 0.toLong),
+      TemporalProjectedExtent(Extent(1, 2, 3, 4), CRS.fromEpsgCode(2004), 1.toLong),
+      TemporalProjectedExtent(Extent(5, 6, 7, 8), CRS.fromEpsgCode(2004), 2.toLong))
+    sc.parallelize(arr)
+  }
+}
--- geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/tests/schemas/TupleWrapper.scala
+++ geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/tests/schemas/TupleWrapper.scala
@@ -8,13 +8,13 @@ import geotrellis.spark._
 import org.apache.spark._
 import org.apache.spark.rdd.RDD
 
-object TupleWrapper extends Wrapper[(ByteArrayTile, Extent)]{
+object TupleWrapper extends Wrapper[(IntArrayTile, Extent)]{
 
-  def testRdd(sc: SparkContext): RDD[(ByteArrayTile, Extent)] = {
+  def testRdd(sc: SparkContext): RDD[(IntArrayTile, Extent)] = {
     val arr = Array(
-      (ByteArrayTile(Array[Byte](0, 1, 2, 3, 4, 5), 2, 3), Extent(0, 0, 1, 1)),
-      (ByteArrayTile(Array[Byte](0, 1, 2, 3, 4, 5), 3, 2), Extent(1, 2, 3, 4)),
-      (ByteArrayTile(Array[Byte](0, 1, 2, 3, 4, 5), 1, 6), Extent(5, 6, 7, 8)))
+      (IntArrayTile(Array[Int](0, 1, 2, 3, 4, 5), 2, 3), Extent(0, 0, 1, 1)),
+      (IntArrayTile(Array[Int](0, 1, 2, 3, 4, 5), 3, 2), Extent(1, 2, 3, 4)),
+      (IntArrayTile(Array[Int](0, 1, 2, 3, 4, 5), 1, 6), Extent(5, 6, 7, 8)))
     sc.parallelize(arr)
   }
 }
