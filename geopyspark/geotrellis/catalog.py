edb918167e6cf621a31d9b142d6a626fb1898e88
James McClain <jmcclain@azavea.com>
2017-01-24 15:37:08 -0500

    Hadoop Catalog API

---
+++ geopyspark/geotrellis/catalog.py
@@ -0,0 +1,93 @@
+from pyspark import SparkConf, SparkContext, RDD
+from pyspark.serializers import AutoBatchedSerializer
+from geopyspark.avroserializer import AvroSerializer
+
+from shapely.geometry import Point, Polygon
+from shapely.wkt import dumps, loads
+
+import time
+import calendar
+
+
+class Metadata:
+    def __init__(self, key_type, value_type, schema, jmetadata):
+        self.key_type = key_type
+        self.value_type = value_type
+        self.schema = schema
+        self.jmetadata = jmetadata
+
+class Catalog:
+
+    def query(self, key_type, value_type, layer_name, layer_zoom, intersects=None):
+        # check parameters
+        if not key_type in ["spatial", "spacetime"]:
+            raise BaseException("Bad key_type")
+        if not value_type in ["singleband", "multiband"]:
+            raise BaseException("Bad value_type")
+        if not isinstance(layer_name, str):
+            raise BaseException("Bad layer_name")
+        if not isinstance(layer_zoom, int):
+            raise BaseException("Bad layer_zoom")
+
+        # perform query
+        if isinstance(intersects, Polygon):
+            tup = self.reader.query(key_type, value_type, layer_name, layer_zoom, dumps(intersects), [])
+        elif isinstance(intersects, str):
+            tup = self.reader.query(key_type, value_type, layer_name, layer_zoom, intersects, [])
+        elif intersects == None:
+            tup = self.reader.read(key_type, value_type, layer_name, layer_zoom)
+        else:
+            raise BaseException("Bad polygon")
+
+        # construct metadata
+        schema = tup._2()
+        if key_type == "spatial":
+            jmetadata = self.store.metadataSpatial(layer_name, layer_zoom)
+        else:
+            jmetadata = self.store.metadataSpacetime(layer_name, layer_zoom)
+        metadata = Metadata(key_type, value_type, schema, jmetadata)
+
+        # construct rdd
+        jrdd = tup._1()
+        serializer = AvroSerializer(schema)
+        rdd = RDD(jrdd, self.sc, AutoBatchedSerializer(serializer))
+
+        # return rdd and metadata
+        return (rdd, metadata)
+
+    def write(self, layer_name, layer_zoom, rdd, metadata, time_unit=None, index_strategy="zorder"):
+        # check parameters
+        if not isinstance(layer_name, str):
+            raise BaseException("Bad layer_name")
+        if not isinstance(layer_zoom, int):
+            raise BaseException("Bad layer_zoom")
+        if not isinstance(rdd, RDD):
+            raise BaseException("Bad rdd")
+        if not isinstance(metadata, Metadata):
+            raise BaseException("Bad metadata")
+        if not (isinstance(time_unit, str) or time_unit == None):
+            raise BaseException("Bad time_unit")
+        if not index_strategy in ["zorder", "hilbert", "rowmajor"]:
+            raise BaseException("Bad index_strategy")
+
+        # gather parameters for call to Scala method
+        key_type = metadata.key_type
+        value_type = metadata.value_type
+        schema = metadata.schema
+        jmetadata = metadata.jmetadata
+        if time_unit == None:
+            time_unit = ""
+
+        # write the data
+        self.writer.write(key_type, value_type, layer_name, layer_zoom, rdd._jrdd, jmetadata, schema, time_unit, index_strategy)
+
+class HadoopCatalog(Catalog):
+
+    def __init__(self, uri, sc):
+        self.sc = sc
+        store_factory = sc._gateway.jvm.geopyspark.geotrellis.io.AttributeStoreFactory
+        self.store = store_factory.buildHadoop(uri, sc._jsc.sc())
+        reader_factory = sc._gateway.jvm.geopyspark.geotrellis.io.LayerReaderFactory
+        self.reader = reader_factory.buildHadoop(self.store)
+        writer_factory = sc._gateway.jvm.geopyspark.geotrellis.io.LayerWriterFactory
+        self.writer = writer_factory.buildHadoop(self.store)
--- geopyspark/tests/hdfs_layer_test.py
+++ geopyspark/tests/hdfs_layer_test.py
@@ -1,16 +1,12 @@
 #!/bin/env python3
 
 import sys
-
-from pyspark import SparkConf, SparkContext, RDD
-from pyspark.serializers import AutoBatchedSerializer
-from geopyspark.avroserializer import AvroSerializer
-
-from shapely.geometry import Point, Polygon
-from shapely.wkt import dumps, loads
-
-import time
 import calendar
+import time
+
+from pyspark import SparkContext
+from shapely.geometry import Polygon
+from geopyspark.geotrellis.catalog import HadoopCatalog
 
 
 if __name__ == "__main__":
@@ -21,24 +17,10 @@ if __name__ == "__main__":
     else:
         exit(-1)
 
-    sc = SparkContext(appName="hdfs-layer-test")
-
-    store_factory = sc._gateway.jvm.geopyspark.geotrellis.io.AttributeStoreFactory
-    store = store_factory.buildHadoop(uri, sc._jsc.sc())
-    metadata = store.metadataSpatial(layer_name, layer_zoom)
-    print(metadata.get())
-
-    reader_factory = sc._gateway.jvm.geopyspark.geotrellis.io.LayerReaderFactory
-    reader = reader_factory.buildHadoop(store)
     polygon = Polygon([(-87.1875, 34.43409789359469), (-78.15673828125, 34.43409789359469), (-78.15673828125, 39.87601941962116), (-87.1875, 39.87601941962116)])
-    tup = reader.query("spatial", "singleband", layer_name, layer_zoom, dumps(polygon), [])
-    # tup = reader.read("spatial", "singleband", layer_name, layer_zoom)
-    (jrdd, schema) = (tup._1(), tup._2())
-    serializer = AvroSerializer(schema)
-    rdd = RDD(jrdd, sc, AutoBatchedSerializer(serializer))
-    print(rdd.take(1))
 
-    writer_factory = sc._gateway.jvm.geopyspark.geotrellis.io.LayerWriterFactory
-    writer = writer_factory.buildHadoop(store)
+    sc = SparkContext(appName="hdfs-layer-test")
+    catalog = HadoopCatalog(uri, sc)
+    (rdd, metadata) = catalog.query("spatial", "singleband", layer_name, layer_zoom, polygon)
     new_layer_name = layer_name + "-" + str(calendar.timegm(time.gmtime()))
-    writer.write("spatial", "singleband", "", new_layer_name, 0, rdd._jrdd, schema, metadata)
+    catalog.write(new_layer_name, layer_zoom, rdd, metadata)
