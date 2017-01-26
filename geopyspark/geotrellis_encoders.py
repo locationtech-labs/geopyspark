fabc50fae1f4bc4fad60f60236a2d2602e6aa452
jbouffard <jbouffard@azavea.com>
2017-01-26 14:40:03 -0500

    Created the GeoTrellisEncoder class

--- geopyspark/geotrellis_encoders.py
+++ geopyspark/geotrellis_encoders.py
@@ -10,160 +10,160 @@ import numpy as np
 import array
 
 
-def tile_encoder(obj):
-    (r, c) = obj.shape
+class GeoTrellisEncoder(object):
+    def __init__(self, custom_class=None, custom_encoder=None):
 
-    if obj.dtype.type == np.int8 or obj.dtype.type == np.uint8:
-        values = array.array('B', obj.flatten()).tostring()
-    else:
-        values = obj.flatten().tolist()
+        self.custom_class = custom_class
+        self.custom_encoder = custom_encoder
 
-    if isinstance(obj, TileArray):
+    @staticmethod
+    def tile_encoder(obj):
+        (r, c) = obj.shape
+
+        if obj.dtype.type == np.int8 or obj.dtype.type == np.uint8:
+            values = array.array('B', obj.flatten()).tostring()
+        else:
+            values = obj.flatten().tolist()
+
+        if isinstance(obj, TileArray):
+            datum = {
+                    'cols': c,
+                    'rows': r,
+                    'cells': values,
+                    'noDataValue': obj.no_data_value
+                    }
+        else:
+            datum = {
+                    'cols': c,
+                    'rows': r,
+                    'cells': values
+                    }
+
+        return datum
+
+    @staticmethod
+    def extent_encoder(obj):
         datum = {
-                'cols': c,
-                'rows': r,
-                'cells': values,
-                'noDataValue': obj.no_data_value
+                'xmin': obj.xmin,
+                'xmax': obj.xmax,
+                'ymin': obj.ymin,
+                'ymax': obj.ymax
                 }
-    else:
+
+        return datum
+
+    def projected_extent_encoder(self, obj):
         datum = {
-                'cols': c,
-                'rows': r,
-                'cells': values
+                'extent': self.extent_encoder(obj.extent),
+                'epsg': obj.epsg_code
                 }
 
-    return datum
+        return datum
 
-def extent_encoder(obj):
-    datum = {
-            'xmin': obj.xmin,
-            'xmax': obj.xmax,
-            'ymin': obj.ymin,
-            'ymax': obj.ymax
-            }
+    def temporal_projected_extent_encoder(self, obj):
+        datum = {
+                'extent': self.extent_encoder(obj.extent),
+                'epsg': obj.epsg_code,
+                'instant': obj.instant
+                }
 
-    return datum
+        return datum
 
-def projected_extent_encoder(obj):
-    datum = {
-            'extent': extent_encoder(obj.extent),
-            'epsg': obj.epsg_code
-            }
+    @staticmethod
+    def spatial_key_encoder(obj):
+        datum = {
+                'col': obj.col,
+                'row': obj.row
+                }
 
-    return datum
+        return datum
 
-def temporal_projected_extent_encoder(obj):
-    datum = {
-            'extent': extent_encoder(obj.extent),
-            'epsg': obj.epsg_code,
-            'instant': obj.instant
-            }
+    @staticmethod
+    def spacetime_key_encoder(obj):
+        datum = {
+                'col': obj.col,
+                'row': obj.row,
+                'instant': obj.instant
+                }
 
-    return datum
+        return datum
 
-def spatial_key_encoder(obj):
-    datum = {
-            'col': obj.col,
-            'row': obj.row
-            }
+    def multiband_encoder(self, obj):
+        tile_datums = list(map(self.tile_encoder, obj))
 
-    return datum
+        datum = {
+                'bands': tile_datums
+                }
 
-def spacetime_key_encoder(obj):
-    datum = {
-            'col': obj.col,
-            'row': obj.row,
-            'instant': obj.instant
-            }
+        return datum
 
-    return datum
+    @staticmethod
+    def tuple_encoder(obj, encoder_1, encoder_2):
+        (a, b) = obj
 
-def multiband_encoder(obj):
-    tile_datums = list(map(tile_encoder, obj))
+        datum_1 = encoder_1(a)
+        datum_2 = encoder_2(b)
 
-    datum = {
-            'bands': tile_datums
-            }
+        datum = {
+                '_1': datum_1,
+                '_2': datum_2
+                }
 
-    return datum
+        return datum
 
-def tuple_encoder(obj, encoders):
-    (a, b) = obj
-    (encoder_1, encoder_2) = encoders
+    def key_value_record_encoder(self, obj):
+        encoder = self.tuple_encoder_creator(obj[0])
 
-    datum_1 = encoder_1(a)
-    datum_2 = encoder_2(b)
+        tuple_datums = list(map(encoder, obj))
 
-    datum = {
-            '_1': datum_1,
-            '_2': datum_2
-            }
+        datum = {
+                'pairs': tuple_datums
+                }
 
-    return datum
+        return datum
 
-def key_value_record_encoder(obj, custom_class=None, custom_encoder=None):
-    encoder = tuple_encoder_creator(obj[0],
-            custom_class=custom_class,
-            custom_encoder=custom_encoder)
+    def tuple_encoder_creator(self, obj):
+        (a, b) = obj
 
-    tuple_datums = list(map(encoder, obj))
+        encoder_1 = self.get_encoder(a)
+        encoder_2 = self.get_encoder(b)
 
-    datum = {
-            'pairs': tuple_datums
-            }
+        return partial(self.tuple_encoder,
+                encoder_1=encoder_1,
+                encoder_2=encoder_2)
 
-    return datum
+    def get_encoder(self, obj):
 
-def tuple_encoder_creator(obj, custom_class=None, custom_encoder=None):
-    (a, b) = obj
+        if self.custom_class is not None and self.custom_encoder is not None:
+            if isinstance(obj, self.custom_class):
+                return self.custom_encoder
 
-    # TODO: FIND A BETTER WAY TO CHECK FOR CUSTOM TYPES IN TUPLES
-    encoder_1 = get_encoder(a,
-            custom_class=custom_class,
-            custom_encoder=custom_encoder)
+        elif isinstance(obj, list) and isinstance(obj[0], tuple):
+            return self.key_value_record_encoder
 
-    encoder_2 = get_encoder(b,
-            custom_class=custom_class,
-            custom_encoder=custom_encoder)
+        elif isinstance(obj, tuple):
+            return self.tuple_encoder_creator
 
-    return partial(tuple_encoder, encoders=(encoder_1, encoder_2))
+        elif isinstance(obj, list):
+            return self.multiband_encoder
 
-def get_encoder(obj, custom_class=None, custom_encoder=None):
+        elif isinstance(obj, TileArray) or isinstance(obj, np.ndarray):
+            return self.tile_encoder
 
-    if custom_class is not None and custom_encoder is not None:
-        if isinstance(obj, custom_class):
-            return custom_encoder
+        elif isinstance(obj, Extent):
+            return self.extent_encoder
 
-    elif isinstance(obj, list) and isinstance(obj[0], tuple):
-        return partial(key_value_record_encoder,
-                custom_class=custom_class,
-                custom_encoder=custom_encoder)
+        elif isinstance(obj, ProjectedExtent):
+            return self.projected_extent_encoder
 
-    elif isinstance(obj, tuple):
-        return tuple_encoder_creator(obj,
-                custom_class=custom_class,
-                custom_encoder=custom_encoder)
+        elif isinstance(obj, TemporalProjectedExtent):
+            return self.temporal_projected_extent_encoder
 
-    elif isinstance(obj, list):
-        return multiband_encoder
+        elif isinstance(obj, SpatialKey):
+            return self.spatial_key_encoder
 
-    elif isinstance(obj, TileArray) or isinstance(obj, np.ndarray):
-        return tile_encoder
+        elif isinstance(obj, SpaceTimeKey):
+            return self.spacetime_key_encoder
 
-    elif isinstance(obj, Extent):
-        return extent_encoder
-
-    elif isinstance(obj, ProjectedExtent):
-        return projected_extent_encoder
-
-    elif isinstance(obj, TemporalProjectedExtent):
-        return temporal_projected_extent_encoder
-
-    elif isinstance(obj, SpatialKey):
-        return spatial_key_encoder
-
-    elif isinstance(obj, SpaceTimeKey):
-        return spacetime_key_encoder
-
-    else:
-        raise Exception('Could not find encoder for', obj)
+        else:
+            raise Exception('Could not find encoder for', obj)
