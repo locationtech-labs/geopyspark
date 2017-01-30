c2dfec369fa77985b159398e116531366c092956
jbouffard <jbouffard@azavea.com>
2017-01-30 14:16:38 -0500

    Merged s3_geotiff_rdd and hadoop_geotiff_rdd into geotiff_rdd
    Updated the hadoop_geotiff_rdd_test

--- geopyspark/geotrellis/geotiff_rdd.py
+++ geopyspark/geotrellis/geotiff_rdd.py
@@ -4,19 +4,7 @@ from py4j.java_gateway import java_import
 from geopyspark.avroserializer import AvroSerializer
 
 
-class HadoopGeoTiffRDD(object):
-    def __init__(self, pysc):
-
-        self.pysc = pysc
-        self._sc = self.pysc._jsc.sc()
-
-        java_import(self.pysc._gateway.jvm,
-                "geopyspark.geotrellis.io.hadoop.HadoopGeoTiffRDDWrapper")
-
-        self._hadoop_wrapper = self.pysc._gateway.jvm.HadoopGeoTiffRDDWrapper
-
-        self.custom_class = None
-
+class GeoTiffRDD:
     def set_custom_class(self, custom_class):
         self.custom_class = custom_class
 
@@ -36,6 +24,20 @@ class HadoopGeoTiffRDD(object):
             ser = AvroSerializer(schema, geotrellis_decoder, geotrellis_encoder)
 
         return RDD(java_rdd, self.pysc, AutoBatchedSerializer(ser))
+
+
+class HadoopGeoTiffRDD(GeoTiffRDD):
+    def __init__(self, pysc):
+
+        self.pysc = pysc
+        self._sc = self.pysc._jsc.sc()
+
+        java_import(self.pysc._gateway.jvm,
+                "geopyspark.geotrellis.io.hadoop.HadoopGeoTiffRDDWrapper")
+
+        self._hadoop_wrapper = self.pysc._gateway.jvm.HadoopGeoTiffRDDWrapper
+
+        self.custom_class = None
 
     def get_spatial(self, path, options=None):
         if options is None:
@@ -66,5 +68,51 @@ class HadoopGeoTiffRDD(object):
             result = self._hadoop_wrapper.temporalMultiband(path, self._sc)
         else:
             result = self._hadoop_wrapper.temporalMultiband(path, options, self._sc)
+
+        return self._return_rdd(result._1(), result._2())
+
+
+class S3GeoTiffRDD(GeoTiffRDD):
+    def __init__(self, pysc):
+
+        self.pysc = pysc
+        self._sc = self.pysc._jsc.sc()
+
+        java_import(self.pysc._gateway.jvm,
+                "geopyspark.geotrellis.io.s3.S3GeoTiffRDDWrapper")
+
+        self._s3_wrapper = self.pysc._gateway.jvm.S3GeoTiffRDDWrapper
+
+        self.custom_class = None
+
+    def get_spatial(self, bucket, prefix, options=None):
+        if options is None:
+            result = self._s3_wrapper.spatial(bucket, prefix, self._sc)
+        else:
+            result = self._s3_wrapper.spatial(bucket, prefix, options, self._sc)
+
+        return self._return_rdd(result._1(), result._2())
+
+    def get_spatial_multiband(self, bucket, prefix, options=None):
+        if options is None:
+            result = self._s3_wrapper.spatialMultiband(bucket, prefix, self._sc)
+        else:
+            result = self._s3_wrapper.spatialMultiband(bucket, prefix, options, self._sc)
+
+        return self._return_rdd(result._1(), result._2())
+
+    def get_temporal(self, bucket, prefix, options=None):
+        if options is None:
+            result = self._s3_wrapper.temporal(bucket, prefix, self._sc)
+        else:
+            result = self._s3_wrapper.temporal(bucket, prefix, options, self._sc)
+
+        return self._return_rdd(result._1(), result._2())
+
+    def get_temporal_multiband(self, bucket, prefix, options=None):
+        if options is None:
+            result = self._s3_wrapper.temporalMultiband(bucket, prefix, self._sc)
+        else:
+            result = self._s3_wrapper.temporalMultiband(bucket, prefix, options, self._sc)
 
         return self._return_rdd(result._1(), result._2())
