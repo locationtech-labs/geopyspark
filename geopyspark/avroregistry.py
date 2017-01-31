04cdb227c6fdcadc80e1485cbebf04c5c385c4c5
jbouffard <jbouffard@azavea.com>
2017-01-31 10:34:00 -0500

    Changed how keys are created in the AvroRegistry

--- geopyspark/avroregistry.py
+++ geopyspark/avroregistry.py
@@ -2,7 +2,7 @@ custom_decoders = {}
 custom_encoders = {}
 
 def add_decoder(custom_cls, decoding_method):
-    custom_decoders[custom_cls.__name__] = decoding_method
+    custom_decoders[type(custom_cls).__name__] = decoding_method
 
 def add_encoder(custom_cls, encoding_method):
-    custom_encoders[custom_cls.__name__] = encoding_method
+    custom_encoders[type(custom_cls).__name__] = encoding_method
