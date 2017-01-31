e1a11249445fcb2f2760917a7c9e268da8a1cf1f
jbouffard <jbouffard@azavea.com>
2017-01-31 15:42:14 -0500

    Updated geotiff_rdd to use AvroRegistry

--- geopyspark/geotrellis/geotiff_rdd.py
+++ geopyspark/geotrellis/geotiff_rdd.py
@@ -2,19 +2,25 @@ from pyspark import SparkConf, SparkContext, RDD
 from pyspark.serializers import AutoBatchedSerializer
 from py4j.java_gateway import java_import
 from geopyspark.avroserializer import AvroSerializer
+from geopyspark.avroregistry import AvroRegistry
 
 
 class GeoTiffRDD:
-    def _return_rdd(self, java_rdd, schema):
-        ser = AvroSerializer(schema)
+    def _decode_java_rdd(self, java_rdd, schema, avroregistry):
+        if avroregistry is None:
+            ser = AvroSerializer(schema)
+        else:
+            ser = AvroSerializer(schema, avroregistry)
 
         return RDD(java_rdd, self.pysc, AutoBatchedSerializer(ser))
 
 
 class HadoopGeoTiffRDD(GeoTiffRDD):
-    def __init__(self, pysc):
+    def __init__(self, pysc, avroregistry=None):
 
         self.pysc = pysc
+        self.avroregistry = avroregistry
+
         self._sc = self.pysc._jsc.sc()
 
         java_import(self.pysc._gateway.jvm,
@@ -28,7 +34,7 @@ class HadoopGeoTiffRDD(GeoTiffRDD):
         else:
             result = self._hadoop_wrapper.spatial(path, options, self._sc)
 
-        return self._decode_java_rdd(result._1(), result._2())
+        return self._decode_java_rdd(result._1(), result._2(), self.avroregistry)
 
     def get_spatial_multiband(self, path, options=None):
         if options is None:
@@ -36,7 +42,7 @@ class HadoopGeoTiffRDD(GeoTiffRDD):
         else:
             result = self._hadoop_wrapper.spatialMultiband(path, options, self._sc)
 
-        return self._decode_java_rdd(result._1(), result._2())
+        return self._decode_java_rdd(result._1(), result._2(), self.avroregistry)
 
     def get_temporal(self, path, options=None):
         if options is None:
@@ -44,7 +50,7 @@ class HadoopGeoTiffRDD(GeoTiffRDD):
         else:
             result = self._hadoop_wrapper.temporal(path, options, self._sc)
 
-        return self._decode_java_rdd(result._1(), result._2())
+        return self._decode_java_rdd(result._1(), result._2(), self.avroregistry)
 
     def get_temporal_multiband(self, path, options=None):
         if options is None:
@@ -52,13 +58,15 @@ class HadoopGeoTiffRDD(GeoTiffRDD):
         else:
             result = self._hadoop_wrapper.temporalMultiband(path, options, self._sc)
 
-        return self._decode_java_rdd(result._1(), result._2())
+        return self._decode_java_rdd(result._1(), result._2(), self.avroregistry)
 
 
 class S3GeoTiffRDD(GeoTiffRDD):
-    def __init__(self, pysc):
+    def __init__(self, pysc, avroregistry=None):
 
         self.pysc = pysc
+        self.avroregistry = avroregistry
+
         self._sc = self.pysc._jsc.sc()
 
         java_import(self.pysc._gateway.jvm,
@@ -72,7 +80,7 @@ class S3GeoTiffRDD(GeoTiffRDD):
         else:
             result = self._s3_wrapper.spatial(bucket, prefix, options, self._sc)
 
-        return self._decode_java_rdd(result._1(), result._2())
+        return self._decode_java_rdd(result._1(), result._2(), self.avroregistry)
 
     def get_spatial_multiband(self, bucket, prefix, options=None):
         if options is None:
@@ -80,7 +88,7 @@ class S3GeoTiffRDD(GeoTiffRDD):
         else:
             result = self._s3_wrapper.spatialMultiband(bucket, prefix, options, self._sc)
 
-        return self._decode_java_rdd(result._1(), result._2())
+        return self._decode_java_rdd(result._1(), result._2(), self.avroregistry)
 
     def get_temporal(self, bucket, prefix, options=None):
         if options is None:
@@ -88,7 +96,7 @@ class S3GeoTiffRDD(GeoTiffRDD):
         else:
             result = self._s3_wrapper.temporal(bucket, prefix, options, self._sc)
 
-        return self._decode_java_rdd(result._1(), result._2())
+        return self._decode_java_rdd(result._1(), result._2(), self.avroregistry)
 
     def get_temporal_multiband(self, bucket, prefix, options=None):
         if options is None:
@@ -96,4 +104,4 @@ class S3GeoTiffRDD(GeoTiffRDD):
         else:
             result = self._s3_wrapper.temporalMultiband(bucket, prefix, options, self._sc)
 
-        return self._decode_java_rdd(result._1(), result._2())
+        return self._decode_java_rdd(result._1(), result._2(), self.avroregistry)
