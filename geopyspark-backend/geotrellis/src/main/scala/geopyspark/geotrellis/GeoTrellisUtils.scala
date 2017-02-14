5776b5201f36b74d9e61a24f26279513bb20817b
jbouffard <jbouffard@azavea.com>
2017-02-14 14:49:56 -0500

    Added GeoTrellisUtils to the geotrellis subproject

---
+++ geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/GeoTrellisUtils.scala
@@ -0,0 +1,154 @@
+package geopyspark.geotrellis
+
+import geotrellis.proj4._
+import geotrellis.raster._
+import geotrellis.vector._
+import geotrellis.spark._
+import geotrellis.spark.tiling._
+
+import org.apache.spark.rdd._
+
+import scala.collection.JavaConverters._
+import collection.JavaConversions._
+import java.util.Map
+
+
+object GeoTrellisUtils {
+  def convertToScalaMap(
+    javaMap: java.util.Map[String, Any],
+    stringValues: Array[String]
+  ): (scala.collection.Map[String, String], scala.collection.Map[String, Int]) = {
+    val scalaMap = javaMap.asScala
+
+    val intMap =
+      scalaMap.filterKeys(x => !(stringValues.contains(x)))
+        .mapValues(x => x.asInstanceOf[Int])
+
+    val stringMap =
+      scalaMap.filterKeys(x => stringValues.contains(x))
+        .mapValues(x => x.asInstanceOf[String])
+
+    (stringMap, intMap)
+  }
+
+  implicit class JavaArrayListExtensions(al: java.util.ArrayList[java.util.Map[String, _]]) {
+    def toLayoutDefinition: LayoutDefinition =
+      LayoutDefinition(al(0).toExtent, al(1).toTileLayout)
+  }
+
+  implicit class JavaMapExtensions(m: java.util.Map[String, _]) {
+    def toExtent: Extent = {
+      val mappedExtent = m.mapValues(x => x.asInstanceOf[Double])
+      Extent(
+        mappedExtent("xmin"),
+        mappedExtent("ymin"),
+        mappedExtent("xmax"),
+        mappedExtent("ymax"))
+    }
+
+    def toTileLayout: TileLayout = {
+      val mappedLayout = m.mapValues(x => x.asInstanceOf[Int])
+
+      TileLayout(
+        mappedLayout("layoutCols"),
+        mappedLayout("layoutRows"),
+        mappedLayout("tileCols"),
+        mappedLayout("tileRows"))
+    }
+
+    def toCrs: Option[CRS] =
+      if (m.isEmpty)
+        None
+      else
+        m.head.asInstanceOf[(String, String)] match {
+          case ("projParams", projParams) => Some(CRS.fromString(projParams))
+          case ("epsg", epsgString) => Some(CRS.fromName(s"EPSG:$epsgString"))
+          case ("wktString", wktString) => Some(CRS.fromWKT(wktString))
+          case (k, _) => throw new Exception(s"Cannot create CRS from $k")
+        }
+
+    def toTileLayerMetadata[K: Boundable, V](rdd: RDD[(K, V)]): TileLayerMetadata[K] = {
+      val cellType = CellType.fromString(m("cellType") .asInstanceOf[String])
+
+      val layout = m("layout")
+        .asInstanceOf[java.util.ArrayList[java.util.Map[String, _]]]
+        .toLayoutDefinition
+
+      val extent = m("extent").asInstanceOf[java.util.Map[String, _]].toExtent
+
+      val crs = CRS.fromString(m("crs").asInstanceOf[String])
+
+      val bounds = Bounds.fromRdd(rdd)
+
+      TileLayerMetadata(
+        cellType,
+        layout,
+        extent,
+        crs,
+        bounds
+      )
+    }
+  }
+
+
+  implicit class ScalaMapExtensions(m: scala.collection.immutable.Map[String, Any]) {
+    def toJavaMap: java.util.Map[String, Any] = {
+      val convertedInner =
+        m.mapValues(x =>
+            x match {
+              case a: scala.collection.immutable.Map[_, _] => mapAsJavaMap(a)
+              case b: List[_] =>
+                val result = b.map(value =>
+                    mapAsJavaMap(value
+                      .asInstanceOf[scala.collection.immutable.Map[String, _]]))
+
+                result.asJava
+
+              // If other types aren't catched, they'll either return as a
+              // JavaObject or not at all
+              case c: String  => c
+              case d: Int => d
+              case e: Double => e
+            })
+
+      mapAsJavaMap(convertedInner)
+    }
+  }
+
+
+  implicit class ExtentExtensions(e: Extent) {
+    def toMap: scala.collection.immutable.Map[String, Double] =
+      scala.collection.immutable.Map(
+        "xmin" -> e.xmin,
+        "ymin" -> e.ymin,
+        "xmax" -> e.xmax,
+        "ymax" -> e.ymax)
+  }
+
+
+  implicit class TileLayoutExtensions(tl: TileLayout) {
+    def toMap: scala.collection.immutable.Map[String, Int] =
+      scala.collection.immutable.Map(
+        "layoutCols" -> tl.layoutCols,
+        "layoutRows" -> tl.layoutRows,
+        "tileCols" -> tl.tileCols,
+        "tileRows" -> tl.tileRows)
+  }
+
+
+  implicit class LayoutDefinitionExtensions(ld: LayoutDefinition) {
+    def toList = List(ld.extent.toMap, ld.tileLayout.toMap)
+  }
+
+
+  implicit class TileLayerMetadataExtensions[K](m: TileLayerMetadata[K]) {
+    def toJavaMap: java.util.Map[String, Any] =
+      scala.collection.immutable.Map(
+        "cellType" -> m.cellType.name,
+        "layout" -> m.layout.toList,
+        "extent" -> m.extent.toMap,
+        "crs" -> m.crs.toProj4String,
+        "bounds" -> m.bounds.get.toString
+      ).toJavaMap
+  }
+}
