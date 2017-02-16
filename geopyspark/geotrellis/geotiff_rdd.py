619d7113ad6f6ede72046445e7674668ee577a9b
jbouffard <jbouffard@azavea.com>
2017-02-16 17:02:34 -0500

    Updated GeoTiffRDD classes to use GeoPyContext

--- geopyspark/geotrellis/geotiff_rdd.py
+++ geopyspark/geotrellis/geotiff_rdd.py
@@ -3,6 +3,7 @@ from pyspark.serializers import AutoBatchedSerializer
 from py4j.java_gateway import java_import
 from geopyspark.avroserializer import AvroSerializer
 from geopyspark.avroregistry import AvroRegistry
+from geopyspark.geopycontext import GeoPyContext
 
 
 class GeoTiffRDD:
@@ -12,51 +13,46 @@ class GeoTiffRDD:
         else:
             ser = AvroSerializer(schema, avroregistry)
 
-        return RDD(java_rdd, self.pysc, AutoBatchedSerializer(ser))
+        return RDD(java_rdd, self.geopysc.pysc, AutoBatchedSerializer(ser))
 
 
 class HadoopGeoTiffRDD(GeoTiffRDD):
     def __init__(self, pysc, avroregistry=None):
+        self.geopysc = GeoPyContext(pysc)
 
-        self.pysc = pysc
         self.avroregistry = avroregistry
 
-        self._sc = self.pysc._jsc.sc()
-
-        java_import(self.pysc._gateway.jvm,
-                "geopyspark.geotrellis.io.hadoop.HadoopGeoTiffRDDWrapper")
-
-        self._hadoop_wrapper = self.pysc._gateway.jvm.HadoopGeoTiffRDDWrapper
+        self._hadoop_wrapper = self.geopysc.hadoop_geotiff_rdd
 
     def get_spatial(self, path, options=None):
         if options is None:
-            result = self._hadoop_wrapper.readSpatialSingleband(path, self._sc)
+            result = self._hadoop_wrapper.readSpatialSingleband(path, self.geopysc.sc)
         else:
-            result = self._hadoop_wrapper.readSpatialSingleband(path, options, self._sc)
+            result = self._hadoop_wrapper.readSpatialSingleband(path, options, self.geopysc.sc)
 
         return self._decode_java_rdd(result._1(), result._2(), self.avroregistry)
 
     def get_spatial_multiband(self, path, options=None):
         if options is None:
-            result = self._hadoop_wrapper.readSpatialMultiband(path, self._sc)
+            result = self._hadoop_wrapper.readSpatialMultiband(path, self.geopysc.sc)
         else:
-            result = self._hadoop_wrapper.readSpatialMultiband(path, options, self._sc)
+            result = self._hadoop_wrapper.readSpatialMultiband(path, options, self.geopysc.sc)
 
         return self._decode_java_rdd(result._1(), result._2(), self.avroregistry)
 
     def get_spacetime(self, path, options=None):
         if options is None:
-            result = self._hadoop_wrapper.readSpaceTimeSingleband(path, self._sc)
+            result = self._hadoop_wrapper.readSpaceTimeSingleband(path, self.geopysc.sc)
         else:
-            result = self._hadoop_wrapper.readSpaceTimeSingleband(path, options, self._sc)
+            result = self._hadoop_wrapper.readSpaceTimeSingleband(path, options, self.geopysc.sc)
 
         return self._decode_java_rdd(result._1(), result._2(), self.avroregistry)
 
     def get_spacetime_multiband(self, path, options=None):
         if options is None:
-            result = self._hadoop_wrapper.readSpaceTimeMultiband(path, self._sc)
+            result = self._hadoop_wrapper.readSpaceTimeMultiband(path, self.geopysc.sc)
         else:
-            result = self._hadoop_wrapper.readSpaceTimeMultiband(path, options, self._sc)
+            result = self._hadoop_wrapper.readSpaceTimeMultiband(path, options, self.geopysc.sc)
 
         return self._decode_java_rdd(result._1(), result._2(), self.avroregistry)
 
@@ -64,44 +60,39 @@ class HadoopGeoTiffRDD(GeoTiffRDD):
 class S3GeoTiffRDD(GeoTiffRDD):
     def __init__(self, pysc, avroregistry=None):
 
-        self.pysc = pysc
+        self.geopysc = GeoPyContext(pysc)
         self.avroregistry = avroregistry
 
-        self._sc = self.pysc._jsc.sc()
-
-        java_import(self.pysc._gateway.jvm,
-                "geopyspark.geotrellis.io.s3.S3GeoTiffRDDWrapper")
-
-        self._s3_wrapper = self.pysc._gateway.jvm.S3GeoTiffRDDWrapper
+        self._s3_wrapper = self.geopysc.s3_geotiff_rdd
 
     def get_spatial(self, bucket, prefix, options=None):
         if options is None:
-            result = self._s3_wrapper.readSpatialSingleband(bucket, prefix, self._sc)
+            result = self._s3_wrapper.readSpatialSingleband(bucket, prefix, self.geopysc.sc)
         else:
-            result = self._s3_wrapper.readSpatialSingleband(bucket, prefix, options, self._sc)
+            result = self._s3_wrapper.readSpatialSingleband(bucket, prefix, options, self.geopysc.sc)
 
         return self._decode_java_rdd(result._1(), result._2(), self.avroregistry)
 
     def get_spatial_multiband(self, bucket, prefix, options=None):
         if options is None:
-            result = self._s3_wrapper.readSpatialMultiband(bucket, prefix, self._sc)
+            result = self._s3_wrapper.readSpatialMultiband(bucket, prefix, self.geopysc.sc)
         else:
-            result = self._s3_wrapper.readSpatialMultiband(bucket, prefix, options, self._sc)
+            result = self._s3_wrapper.readSpatialMultiband(bucket, prefix, options, self.geopysc.sc)
 
         return self._decode_java_rdd(result._1(), result._2(), self.avroregistry)
 
     def get_spacetime(self, bucket, prefix, options=None):
         if options is None:
-            result = self._s3_wrapper.readSpaceTimeSingleband(bucket, prefix, self._sc)
+            result = self._s3_wrapper.readSpaceTimeSingleband(bucket, prefix, self.geopysc.sc)
         else:
-            result = self._s3_wrapper.readSpaceTimeSingleband(bucket, prefix, options, self._sc)
+            result = self._s3_wrapper.readSpaceTimeSingleband(bucket, prefix, options, self.geopysc.sc)
 
         return self._decode_java_rdd(result._1(), result._2(), self.avroregistry)
 
     def get_spacetime_multiband(self, bucket, prefix, options=None):
         if options is None:
-            result = self._s3_wrapper.readSpaceTimeMultiband(bucket, prefix, self._sc)
+            result = self._s3_wrapper.readSpaceTimeMultiband(bucket, prefix, self.geopysc.sc)
         else:
-            result = self._s3_wrapper.readSpaceTimeMultiband(bucket, prefix, options, self._sc)
+            result = self._s3_wrapper.readSpaceTimeMultiband(bucket, prefix, options, self.geopysc.sc)
 
         return self._decode_java_rdd(result._1(), result._2(), self.avroregistry)
