6f321571a5669a10c4ac82211ca3571c7d203158
jbouffard <jbouffard@azavea.com>
2017-02-17 15:38:33 -0500

    Updated and expanded tests for TileLayerMetadata

--- geopyspark/tests/tile_layer_metadata_test.py
+++ geopyspark/tests/tile_layer_metadata_test.py
@@ -7,6 +7,7 @@ from geopyspark.geotrellis.tile_layer_metadata import TileLayerMethods
 from geopyspark.geotrellis.geotiff_rdd import HadoopGeoTiffRDD
 from py4j.java_gateway import java_import
 from geopyspark.avroserializer import AvroSerializer
+from geopyspark.geopycontext import GeoPyContext
 
 import unittest
 import pytest
@@ -15,9 +16,10 @@ import json
 
 class TileLayerMetadataTest(unittest.TestCase):
     def setUp(self):
-        self.pysc = SparkContext(master="local[*]", appName="metadata-test")
-        self.metadata = TileLayerMethods(self.pysc)
-        self.hadoop_geotiff = HadoopGeoTiffRDD(self.pysc)
+        pysc = SparkContext(master="local[*]", appName="metadata-test")
+        self.geopysc = GeoPyContext(pysc)
+        self.metadata = TileLayerMethods(self.geopysc)
+        self.hadoop_geotiff = HadoopGeoTiffRDD(self.geopysc)
 
         self.dir_path = geotiff_test_path("all-ones.tif")
         self.options = {'maxTileSize': 256}
@@ -25,8 +27,8 @@ class TileLayerMetadataTest(unittest.TestCase):
     @pytest.fixture(autouse=True)
     def tearDown(self):
         yield
-        self.pysc.stop()
-        self.pysc._gateway.close()
+        self.geopysc.stop()
+        self.geopysc.close_gateway()
 
     def check_results(self, actual, expected):
         if isinstance(actual, list) and isinstance(expected, list):
@@ -38,7 +40,8 @@ class TileLayerMetadataTest(unittest.TestCase):
             self.assertEqual(actual, expected)
 
     def test_collection(self):
-        (rdd, schema) = self.hadoop_geotiff.get_spatial(self.dir_path)
+        rdd = self.hadoop_geotiff.get_spatial(self.dir_path)
+        schema = rdd.schema
         value = rdd.collect()[0]
 
         projected_extent = value[0]
@@ -63,18 +66,46 @@ class TileLayerMetadataTest(unittest.TestCase):
         actual = [[new_extent, layout], new_extent]
 
         result = self.metadata.collect_metadata(rdd,
-                                                schema,
                                                 new_extent,
                                                 layout,
                                                 epsg_code=value[0].epsg_code)
 
-        result_dict = json.loads(result)
-
-        expected = [[result_dict['layoutDefinition']['extent'],
-                     result_dict['layoutDefinition']['tileLayout']],
-                    result_dict['extent']]
+        expected = [[result['layoutDefinition']['extent'],
+                     result['layoutDefinition']['tileLayout']],
+                    result['extent']]
 
         self.check_results(actual, expected)
+
+    def test_failure(self):
+        rdd = self.hadoop_geotiff.get_spatial(self.dir_path)
+        schema = rdd.schema
+        value = rdd.collect()[0]
+
+        projected_extent = value[0]
+        old_extent = projected_extent.extent
+
+        new_extent = {
+            "xmin": old_extent.xmax,
+            "ymin": old_extent.ymin,
+            "xmax": old_extent.ymin,
+            "ymax": old_extent.ymax
+        }
+
+        (cols, rows) = value[1].shape
+
+        layout = {
+            "layoutCols": 1,
+            "layoutRows": 1,
+            "tileCols": cols,
+            "tileRows": rows
+        }
+
+        self.assertRaises(Exception,
+                          self.metadata.collect_metadata,
+                          rdd,
+                          new_extent,
+                          layout,
+                          epsg_code=value[0].epsg_code)
 
 
 if __name__ == "__main__":
