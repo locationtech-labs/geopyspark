5eafe47670d0df6ed5fb56b6b996e2ffa528a049
jbouffard <jbouffard@azavea.com>
2017-03-10 16:31:20 -0500

    Removed files that are no longer used
    Removed JavaGateway
    
    Removed conf
    
    Removed SingletonBase

--- geopyspark/singleton_base.py
+++
@@ -1,6 +0,0 @@
-class SingletonBase(type):
-    _cls_instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._cls_instances:
-            cls._cls_instances[cls] = super(SingletonBase, cls).__call__(*args, **kwargs)
-        return cls._cls_instances[cls]
