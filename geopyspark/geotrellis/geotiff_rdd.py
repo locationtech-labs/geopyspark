30725857a07a1c5eabafea1f96bd20b32ed67129
jbouffard <jbouffard@azavea.com>
2017-03-15 15:05:37 -0400

    geotiff_rdd now contains a single method, geotiff_rdd

--- geopyspark/geotrellis/geotiff_rdd.py
+++ geopyspark/geotrellis/geotiff_rdd.py
@@ -1,37 +1,43 @@
 from geopyspark.geopycontext import GeoPyContext
 
 
-class HadoopGeoTiffRDD(object):
-    def __init__(self, geopysc):
-        self.geopysc = geopysc
-        self._hadoop_wrapper = self.geopysc.hadoop_geotiff_rdd
+def geotiff_rdd(geopysc,
+                rdd_type,
+                uri,
+                options=None,
+                **kwargs):
 
-    def get_rdd(self, key_type, path, options=None):
-        key = self.geopysc.map_key_input(key_type, False)
+    key = geopysc.map_key_input(rdd_type, False)
 
-        if options is None:
-            result = self._hadoop_wrapper.getRDD(key, path, self.geopysc.sc)
+    if kwargs and not options:
+        options = kwargs
+
+    if 's3' in uri:
+        key_and_bucket_uri = uri.split("s3://")[1]
+        (bucket, prefix) = key_and_bucket_uri.split("/", 1)
+
+        if not options:
+            result = geopysc.s3_geotiff_rdd.getRDD(key,
+                                                   bucket,
+                                                   prefix,
+                                                   geopysc.sc)
         else:
-            result = self._hadoop_wrapper.getRDD(key, path, options, self.geopysc.sc)
+            result = geopysc.s3_geotiff_rdd.getRDD(key,
+                                                   bucket,
+                                                   prefix,
+                                                   options,
+                                                   geopysc.sc)
 
-        ser = self.geopysc.create_tuple_serializer(result._2(), value_type="Tile")
-
-        return self.geopysc.create_python_rdd(result._1(), ser)
-
-
-class S3GeoTiffRDD(object):
-    def __init__(self, geopysc):
-        self.geopysc = geopysc
-        self._s3_wrapper = self.geopysc.s3_geotiff_rdd
-
-    def get_rdd(self, key_type, bucket, prefix, options=None):
-        key = self.geopysc.map_key_input(key_type, False)
-
-        if options is None:
-            result = self._s3_wrapper.getRDD(key, bucket, prefix, self.geopysc.sc)
+    else:
+        if not options:
+            result = geopysc.hadoop_geotiff_rdd.getRDD(key,
+                                                       uri,
+                                                       geopysc.sc)
         else:
-            result = self._s3_wrapper.getRDD(key, bucket, prefix, options, self.geopysc.sc)
+            result = geopysc.hadoop_geotiff_rdd.getRDD(key,
+                                                       uri,
+                                                       options,
+                                                       geopysc.sc)
 
-        ser = self.geopysc.create_tuple_serializer(result._2(), value_type="Tile")
-
-        return self.geopysc.create_python_rdd(result._1(), ser)
+    ser = geopysc.create_tuple_serializer(result._2(), value_type="Tile")
+    return geopysc.create_python_rdd(result._1(), ser)
