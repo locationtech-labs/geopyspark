fe6042fcb27067f937efd8f408656f3f18e123be
James McClain <jmcclain@azavea.com>
2017-03-27 15:11:38 -0400

    Cache pip Libraries

--- docker/scripts/gather-libs.sh
+++ docker/scripts/gather-libs.sh
@@ -14,6 +14,7 @@ tar axvf /archives/gdal-and-friends.tar.gz
 
 # install geopsypark and friends
 cd $HOME/
+chown -R root:root $HOME/.cache/pip
 pip3 install --user -r /scripts/requirements.txt
 pip3 install --user /archives/geopyspark-0.1.0-py3-none-any.whl
 pip3 install --user rasterio==1.0a7
@@ -22,4 +23,4 @@ pip3 install --user rasterio==1.0a7
 cd $HOME/.local/lib/python3.4/site-packages
 tar acvf /archives/geopyspark-and-friends.tar.gz .
 
-chown -R $USER:$GROUP /archives
+chown -R $USER:$GROUP /archives $HOME/.cache/pip
