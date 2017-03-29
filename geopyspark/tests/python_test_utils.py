c6148f49ccb7ceaa03df7300aabca71c5498a03b
James McClain <jmcclain@azavea.com>
2017-03-29 16:03:52 -0400

    Prevent Stale Test Data
    Signed-off-by: James McClain <jmcclain@azavea.com>

--- geopyspark/tests/python_test_utils.py
+++ geopyspark/tests/python_test_utils.py
@@ -1,6 +1,6 @@
 import os
+import shutil
 from os import path
-
 from zipfile import ZipFile as zipped
 
 
@@ -12,7 +12,15 @@ def geotiff_test_path(file_test_path):
 
 def check_directory():
     test_path = "geopyspark/tests/data_files/geotiff_test_files/"
-    if not path.exists(test_path):
-        zip_files = zipped('geopyspark/tests/data_files/geotiff_test_files.zip')
+    if os.path.exists(test_path):
+        test_path_time = os.path.getmtime(test_path)
+    else:
+        test_path_time = 0
+    zip_file = "geopyspark/tests/data_files/geotiff_test_files.zip"
+    zip_file_time = os.path.getmtime(zip_file)
+
+    if test_path_time < zip_file_time:
+        shutil.rmtree(test_path, ignore_errors=True)
+        zip_files = zipped(zip_file)
         zip_files.extractall('geopyspark/tests/data_files/')
         zip_files.close()
