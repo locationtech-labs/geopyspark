53a990ae8c77713eaac470ed528e6d9d7deb4075
James McClain <jmcclain@azavea.com>
2017-03-31 09:12:45 -0400

    One Executor On Travis

--- geopyspark/tests/base_test_class.py
+++ geopyspark/tests/base_test_class.py
@@ -1,7 +1,12 @@
 import unittest
+import os
 
 from geopyspark.geopycontext import GeoPyContext
 
 
 class BaseTestClass(unittest.TestCase):
-    geopysc = GeoPyContext(master="local[1]", appName="test")
+    if 'TRAVIS' in os.environ:
+        master_str = "local[1]"
+    else:
+        master_str = "local[*]"
+    geopysc = GeoPyContext(master=master_str, appName="test")
