984256c87c441f47d8585f8af489e7a618118ad8
James McClain <jmcclain@azavea.com>
2017-04-04 19:53:10 -0400

    Unit Tests For Stitch

---
+++ geopyspark/tests/stitch_test.py
@@ -0,0 +1,50 @@
+import os
+import unittest
+import rasterio
+import numpy as np
+
+from shapely.geometry import Point
+from geopyspark.geotrellis.tile_layer import stitch
+from geopyspark.tests.base_test_class import BaseTestClass
+from geopyspark.geotrellis.constants import SPATIAL
+
+
+class StitchTest(BaseTestClass):
+    geopysc = BaseTestClass.geopysc
+
+    data = np.array([[
+        [1.0, 1.0, 1.0, 1.0, 1.0],
+        [1.0, 1.0, 1.0, 1.0, 1.0],
+        [1.0, 1.0, 1.0, 1.0, 1.0],
+        [1.0, 1.0, 1.0, 1.0, 1.0],
+        [1.0, 1.0, 1.0, 1.0, 0.0]]])
+
+    layer = [({'row': 0, 'col': 0}, {'no_data_value': -1.0, 'data': data}),
+             ({'row': 1, 'col': 0}, {'no_data_value': -1.0, 'data': data}),
+             ({'row': 0, 'col': 1}, {'no_data_value': -1.0, 'data': data}),
+             ({'row': 1, 'col': 1}, {'no_data_value': -1.0, 'data': data})]
+    rdd = geopysc.pysc.parallelize(layer)
+
+    extent = {'xmin': 0.0, 'ymin': 0.0, 'xmax': 33.0, 'ymax': 33.0}
+    layout = {'layoutCols': 2, 'layoutRows': 2, 'tileCols': 5, 'tileRows': 5}
+    metadata = {'cellType': 'float32ud-1.0',
+                'extent': extent,
+                'crs': '+proj=longlat +datum=WGS84 +no_defs ',
+                'bounds': {
+                    'minKey': {'col': 0, 'row': 0},
+                    'maxKey': {'col': 1, 'row': 1}},
+                'layoutDefinition': {
+                    'extent': extent,
+                    'tileLayout': {'tileCols': 5, 'tileRows': 5, 'layoutCols': 2, 'layoutRows': 2}}}
+
+    def test_stitch(self):
+
+        result = stitch(geopysc=self.geopysc,
+                        rdd_type=SPATIAL,
+                        keyed_rdd=self.rdd,
+                        metadata=self.metadata)
+
+        self.assertTrue(result[0]['data'].shape == (1, 10, 10))
+
+if __name__ == "__main__":
+    unittest.main()
