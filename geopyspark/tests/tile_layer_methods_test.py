616ef8dbaed21ad553b9e5baff530a40c9bcc91c
jbouffard <jbouffard@azavea.com>
2017-04-06 16:33:07 -0400

    Updated cut_tiles test to use RasterRDD

--- geopyspark/tests/tile_layer_methods_test.py
+++ geopyspark/tests/tile_layer_methods_test.py
@@ -2,6 +2,7 @@ import os
 import unittest
 import rasterio
 
+from geopyspark.rdd import RasterRDD
 from geopyspark.constants import SPATIAL
 from geopyspark.tests.python_test_utils import check_directory, geotiff_test_path
 from geopyspark.geotrellis.tile_layer import (collect_metadata,
@@ -24,9 +25,10 @@ class TileLayerMethodsTest(BaseTestClass):
     tile = data.read()
     tile_dict = {'data': tile, 'no_data_value': no_data}
 
-    value = hadoop_rdd.collect()[0]
+    value = hadoop_rdd.to_numpy_rdd().collect()[0]
 
     rasterio_rdd = BaseTestClass.geopysc.pysc.parallelize([(value[0], tile_dict)])
+    raster_rdd = RasterRDD.from_numpy_rdd(BaseTestClass.geopysc, SPATIAL, rasterio_rdd)
 
     extent = value[0]['extent']
 
@@ -39,30 +41,20 @@ class TileLayerMethodsTest(BaseTestClass):
         "tileRows": _rows
     }
 
-    metadata = collect_metadata(BaseTestClass.geopysc,
-                                SPATIAL,
-                                hadoop_rdd,
-                                extent,
-                                layout)
+    metadata = hadoop_rdd.collect_metadata(extent=extent, layout=layout)
 
     def test_cut_tiles_hadoop(self):
-        result = cut_tiles(BaseTestClass.geopysc,
-                           SPATIAL,
-                           self.hadoop_rdd,
-                           self.metadata)
+        result = self.hadoop_rdd.cut_tiles(self.metadata)
 
-        (key_bounds, tile) = result.collect()[0]
+        (key_bounds, tile) = result.to_numpy_rdd().collect()[0]
 
         self.assertEqual([0, 0], [key_bounds['col'], key_bounds['row']])
         self.assertTrue((self.value[1]['data'] == tile['data']).all())
 
     def test_cut_tiles_rasterio(self):
-        result = cut_tiles(BaseTestClass.geopysc,
-                           SPATIAL,
-                           self.rasterio_rdd,
-                           self.metadata)
+        result = self.raster_rdd.cut_tiles(self.metadata)
 
-        (key_bounds, tile) = result.collect()[0]
+        (key_bounds, tile) = result.to_numpy_rdd().collect()[0]
 
         self.assertEqual([0, 0], [key_bounds['col'], key_bounds['row']])
         self.assertTrue((self.value[1]['data'] == tile['data']).all())
