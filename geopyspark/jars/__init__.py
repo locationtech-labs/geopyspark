d44fab9de399a8d73fb26331d1b2cb7a338f8c86
jbouffard <jbouffard@azavea.com>
2017-04-12 16:02:14 -0400

    Added doc string to jars/__init__

---
+++ geopyspark/jars/__init__.py
@@ -0,0 +1,6 @@
+"""Contains the jar file(s) needed for GeoPySpark to run.
+
+The backend jar which contains the GeoPySpark scala code must be in this module in order for
+GeoPySpark to run. If there are additional jar files that will be used they can be placed
+in this module as well.
+"""
