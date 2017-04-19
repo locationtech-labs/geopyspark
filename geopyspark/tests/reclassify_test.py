4ce2bac2ecb876d8c2061b4e0d113cdeba7bd07e
jbouffard <jbouffard@azavea.com>
2017-04-19 09:40:57 -0400

    Added reclassify test

---
+++ geopyspark/tests/reclassify_test.py
@@ -0,0 +1,99 @@
+import numpy as np
+import pytest
+import unittest
+
+from geopyspark.geotrellis.rdd import RasterRDD
+from geopyspark.geotrellis.constants import SPATIAL
+from geopyspark.tests.base_test_class import BaseTestClass
+
+
+class ReclassifyTest(BaseTestClass):
+    epsg_code = 3857
+    extent = {'xmin': 0.0, 'ymin': 0.0, 'xmax': 10.0, 'ymax': 10.0}
+
+    projected_extent = {'extent': extent, 'epsg': epsg_code}
+
+    @pytest.fixture(autouse=True)
+    def tearDown(self):
+        yield
+        BaseTestClass.geopysc.pysc._gateway.close()
+
+    def test_all_zeros(self):
+        arr = np.zeros((1, 16, 16))
+        tile = {'data': arr, 'no_data_value': -500}
+
+        rdd = BaseTestClass.geopysc.pysc.parallelize([(self.projected_extent, tile)])
+        raster_rdd = RasterRDD.from_numpy_rdd(BaseTestClass.geopysc, SPATIAL, rdd)
+
+        value_map = {0: 1}
+
+        result = raster_rdd.reclassify(value_map, int).to_numpy_rdd().first()[1]['data']
+
+        self.assertTrue((result == 1).all())
+
+    def test_various_values(self):
+        arr = np.array([[[1, 1, 1, 1],
+                         [2, 2, 2, 2],
+                         [3, 3, 3, 3],
+                         [4, 4, 4, 4]]], dtype=int)
+        tile = {'data': arr, 'no_data_value': -500}
+
+        rdd = BaseTestClass.geopysc.pysc.parallelize([(self.projected_extent, tile)])
+        raster_rdd = RasterRDD.from_numpy_rdd(BaseTestClass.geopysc, SPATIAL, rdd)
+
+        value_map = {1: 10, (2, 3): 17}
+
+        result = raster_rdd.reclassify(value_map, int).to_numpy_rdd().first()[1]['data']
+
+        expected = np.array([[[10, 10, 10, 10],
+                              [17, 17, 17, 17],
+                              [17, 17, 17, 17],
+                              [4, 4, 4, 4]]], dtype=int)
+
+        self.assertTrue((result == expected).all())
+
+    def test_ranges(self):
+        arr = np.array([[[1, 1, 1, 1],
+                         [2, 2, 2, 2],
+                         [3, 3, 3, 3],
+                         [4, 4, 4, 4]]], dtype=int)
+        tile = {'data': arr, 'no_data_value': -500}
+
+        rdd = BaseTestClass.geopysc.pysc.parallelize([(self.projected_extent, tile)])
+        raster_rdd = RasterRDD.from_numpy_rdd(BaseTestClass.geopysc, SPATIAL, rdd)
+
+        value_map = {range(1, 4): 20}
+
+        result = raster_rdd.reclassify(value_map, int).to_numpy_rdd().first()[1]['data']
+
+        expected = np.array([[[20, 20, 20, 20],
+                              [20, 20, 20, 20],
+                              [20, 20, 20, 20],
+                              [4, 4, 4, 4]]], dtype=int)
+
+        self.assertTrue((result == expected).all())
+
+    def test_multibands(self):
+        arr = np.array([[[1, 1, 1, 1]],
+                        [[2, 2, 2, 2]],
+                        [[3, 3, 3, 3]],
+                        [[4, 4, 4, 4]]], dtype=int)
+        tile = {'data': arr, 'no_data_value': -500}
+
+        rdd = BaseTestClass.geopysc.pysc.parallelize([(self.projected_extent, tile)])
+        raster_rdd = RasterRDD.from_numpy_rdd(BaseTestClass.geopysc, SPATIAL, rdd)
+
+        value_map = {(1, 3): 10, (2, 4): 20}
+
+        result = raster_rdd.reclassify(value_map, int).to_numpy_rdd().first()[1]['data']
+
+        expected = np.array([[[10, 10, 10, 10]],
+                             [[20, 20, 20, 20]],
+                             [[10, 10, 10, 10]],
+                             [[20, 20, 20, 20]]], dtype=int)
+
+        self.assertTrue((result == expected).all())
+
+
+if __name__ == "__main__":
+    unittest.main()
