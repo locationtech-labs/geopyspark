5ed5b3efe0c2e41601986b3a36060b68ab2d34cf
jbouffard <jbouffard@azavea.com>
2017-04-21 13:20:29 -0400

    Updated the reclassify tests

--- geopyspark/tests/reclassify_test.py
+++ geopyspark/tests/reclassify_test.py
@@ -5,7 +5,7 @@ import pytest
 import unittest
 
 from geopyspark.geotrellis.rdd import RasterRDD
-from geopyspark.geotrellis.constants import SPATIAL, NODATAINT
+from geopyspark.geotrellis.constants import SPATIAL, NODATAINT, LESSTHAN, GREATERTHAN
 from geopyspark.tests.base_test_class import BaseTestClass
 
 
@@ -43,14 +43,14 @@ class ReclassifyTest(BaseTestClass):
         rdd = BaseTestClass.geopysc.pysc.parallelize([(self.projected_extent, tile)])
         raster_rdd = RasterRDD.from_numpy_rdd(BaseTestClass.geopysc, SPATIAL, rdd)
 
-        value_map = {1: 10, (2, 3): 17}
+        value_map = {1: 10, 3: 17}
 
         result = raster_rdd.reclassify(value_map, int).to_numpy_rdd().first()[1]['data']
 
         expected = np.array([[[10, 10, 10, 10],
                               [17, 17, 17, 17],
                               [17, 17, 17, 17],
-                              [4, 4, 4, 4]]], dtype=int)
+                              [-500, -500, -500, -500]]], dtype=int)
 
         self.assertTrue((result == expected).all())
 
@@ -64,14 +64,14 @@ class ReclassifyTest(BaseTestClass):
         rdd = BaseTestClass.geopysc.pysc.parallelize([(self.projected_extent, tile)])
         raster_rdd = RasterRDD.from_numpy_rdd(BaseTestClass.geopysc, SPATIAL, rdd)
 
-        value_map = {range(1, 4): 20}
+        value_map = {2: 20}
 
-        result = raster_rdd.reclassify(value_map, int).to_numpy_rdd().first()[1]['data']
+        result = raster_rdd.reclassify(value_map, int, GREATERTHAN).to_numpy_rdd().first()[1]['data']
 
-        expected = np.array([[[20, 20, 20, 20],
+        expected = np.array([[[-500, -500, -500, -500],
+                              [-500, -500, -500, -500],
                               [20, 20, 20, 20],
-                              [20, 20, 20, 20],
-                              [4, 4, 4, 4]]], dtype=int)
+                              [20, 20, 20, 20]]], dtype=int)
 
         self.assertTrue((result == expected).all())
 
@@ -85,16 +85,38 @@ class ReclassifyTest(BaseTestClass):
         rdd = BaseTestClass.geopysc.pysc.parallelize([(self.projected_extent, tile)])
         raster_rdd = RasterRDD.from_numpy_rdd(BaseTestClass.geopysc, SPATIAL, rdd)
 
-        value_map = {(1, 3): 10, (2, 4): 20}
+        value_map = {3: 10, 4: 20}
 
         result = raster_rdd.reclassify(value_map, int).to_numpy_rdd().first()[1]['data']
 
         expected = np.array([[[10, 10, 10, 10]],
-                             [[20, 20, 20, 20]],
+                             [[10, 10, 10, 10]],
                              [[10, 10, 10, 10]],
                              [[20, 20, 20, 20]]], dtype=int)
 
         self.assertTrue((result == expected).all())
+
+    def test_floating_voint_ranges(self):
+        arr = np.array([[[0.0, 0.0, 0.0, 0.0],
+                         [1.0, 1.0, 1.0, 1.0],
+                         [1.5, 1.5, 1.5, 1.5],
+                         [2.0, 2.0, 2.0, 2.0]]], dtype=float)
+
+        tile = {'data': arr, 'no_data_value': float('nan')}
+        rdd = BaseTestClass.geopysc.pysc.parallelize([(self.projected_extent, tile)])
+        raster_rdd = RasterRDD.from_numpy_rdd(BaseTestClass.geopysc, SPATIAL, rdd)
+
+        value_map = {2.0: 5.0}
+
+        result = raster_rdd.reclassify(value_map, float, LESSTHAN).to_numpy_rdd().first()[1]['data']
+
+        expected = np.array([[[5.0, 5.0, 5.0, 5.0],
+                              [5.0, 5.0, 5.0, 5.0],
+                              [5.0, 5.0, 5.0, 5.0]]], dtype=float)
+
+        self.assertTrue((result[0, 2, ] == expected).all())
+        for x in result[0, 3, ]:
+            self.assertTrue(math.isnan(x))
 
     def test_no_data_ints(self):
         arr = np.zeros((1, 16, 16))
