85d5ba731dc4e8d2a5a7ff760df9852499e7d1ad
James McClain <jmcclain@azavea.com>
2017-04-28 12:32:54 -0400

    Improve Chances Of Tests Passing On Travis

--- geopyspark/tests/base_test_class.py
+++ geopyspark/tests/base_test_class.py
@@ -12,7 +12,7 @@ check_directory()
 
 class BaseTestClass(unittest.TestCase):
     if 'TRAVIS' in os.environ:
-        master_str = "local[1]"
+        master_str = "local[2]"
     else:
         master_str = "local[*]"
     geopysc = GeoPyContext(master=master_str, appName="test")
