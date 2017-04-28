a5b2c01aa23844adceca1bd3fe81a330e719adba
jpolchlo <jpolchlopek@azavea.com>
2017-04-28 17:18:40 -0400

    Introduce methods for loading multiple GeoTIFFs via `get`
    Signed-off-by: jpolchlo <jpolchlopek@azavea.com>

--- geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/io/geotiff/GeoTiffRDD.scala
+++ geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/io/geotiff/GeoTiffRDD.scala
@@ -3,18 +3,20 @@ package geopyspark.geotrellis.io.geotiff
 import geopyspark.geotrellis._
 
 import geotrellis.proj4._
+import geotrellis.spark.io.avro._
 import geotrellis.spark.io.hadoop._
 import geotrellis.spark.io.s3._
 import geotrellis.spark.io.s3.testkit._
 
 import scala.collection.JavaConverters._
+import scala.collection.JavaConversions._
 
 import java.net.URI
 import java.util.Map
+import scala.reflect._
 
 import org.apache.spark._
 import org.apache.hadoop.fs.Path
-
 
 object GeoTiffRDD {
   import Constants._
@@ -106,8 +108,61 @@ object GeoTiffRDD {
       case S3 =>
         getS3GeoTiffRDD(sc, keyType, uri, S3GeoTiffRDDOptions.setValues(options))
       case _ =>
-        getHadoopGeoTiffRDD(sc, keyType, new Path(path), HadoopGeoTiffRDDOptions.setValues(options))
+        getHadoopGeoTiffRDD(sc, keyType, new Path(uri), HadoopGeoTiffRDDOptions.setValues(options))
     }
+  }
+
+  def get_many(
+    sc: SparkContext,
+    keyType: String,
+    paths: java.util.List[String]
+  ): RasterRDD[_] = {
+    val uris = paths.map{ path => new URI(path) }
+
+    uris
+      .map { uri => 
+        uri.getScheme match {
+          case S3 =>
+            getS3GeoTiffRDD(sc, keyType, uri, S3GeoTiffRDDOptions.default)
+          case _ =>
+            getHadoopGeoTiffRDD(sc, keyType, new Path(uri), HadoopGeoTiffRDDOptions.default)
+        }
+      }
+      .reduce{ (r1, r2) => 
+        keyType match {
+          case PROJECTEDEXTENT =>
+            ProjectedRasterRDD(r1.asInstanceOf[ProjectedRasterRDD].rdd.union(r2.asInstanceOf[ProjectedRasterRDD].rdd))
+          case TEMPORALPROJECTEDEXTENT =>
+            TemporalRasterRDD(r1.asInstanceOf[TemporalRasterRDD].rdd.union(r2.asInstanceOf[TemporalRasterRDD].rdd))
+        }
+      }
+  }
+
+  def get_many(
+    sc: SparkContext,
+    keyType: String,
+    paths: java.util.List[String],
+    options: java.util.Map[String, Any]
+  ): RasterRDD[_] = {
+    val uris = paths.map{ path => new URI(path) }
+
+    uris
+      .map { uri => 
+        uri.getScheme match {
+          case S3 =>
+            getS3GeoTiffRDD(sc, keyType, uri, S3GeoTiffRDDOptions.setValues(options))
+          case _ =>
+            getHadoopGeoTiffRDD(sc, keyType, new Path(uri), HadoopGeoTiffRDDOptions.setValues(options))
+        }
+      }
+      .reduce{ (r1, r2) => 
+        keyType match {
+          case PROJECTEDEXTENT =>
+            ProjectedRasterRDD(r1.asInstanceOf[ProjectedRasterRDD].rdd.union(r2.asInstanceOf[ProjectedRasterRDD].rdd))
+          case TEMPORALPROJECTEDEXTENT =>
+            TemporalRasterRDD(r1.asInstanceOf[TemporalRasterRDD].rdd.union(r2.asInstanceOf[TemporalRasterRDD].rdd))
+        }
+      }
   }
 
   private def getHadoopGeoTiffRDD(
