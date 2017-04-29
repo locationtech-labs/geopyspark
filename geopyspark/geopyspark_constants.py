91d658d52907e81bd1374c3b318cf2811accbffc
jbouffard <jbouffard@azavea.com>
2017-04-29 14:01:57 -0400

    Added docstrings to geopyspark_constants

--- geopyspark/geopyspark_constants.py
+++ geopyspark/geopyspark_constants.py
@@ -1,5 +1,11 @@
+"""GeoPySpark package constants."""
 from os import path
 
+"""GeoPySpark version."""
 VERSION = '0.1.0'
+
+"""Backend jar name."""
 JAR = 'geotrellis-backend-assembly-' + VERSION + '.jar'
+
+"""The current location of this file."""
 CWD = path.abspath(path.dirname(__file__))
