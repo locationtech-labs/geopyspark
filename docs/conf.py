1898299b2db99349f78b8741853028ab5de96f6a
jbouffard <jbouffard@azavea.com>
2017-05-03 08:41:39 -0400

    Cleaned up conf

--- docs/conf.py
+++ docs/conf.py
@@ -97,7 +97,6 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
 
 import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
@@ -112,7 +111,6 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
 #html_static_path = ['_static']
 
 
@@ -171,6 +169,3 @@ texinfo_documents = [
      author, 'GeoPySpark', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
