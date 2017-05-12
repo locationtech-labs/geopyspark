779dbf6d9d6db359b2f2c7fff2d497a1b380d865
jpolchlo <jpolchlopek@azavea.com>
2017-05-12 10:26:39 -0400

    Respond to PR comments
    Signed-off-by: jpolchlo <jpolchlopek@azavea.com>

--- geopyspark/geotrellis/render.py
+++ geopyspark/geotrellis/render.py
@@ -1,16 +1,53 @@
-from geopyspark.geotrellis.constants import NEARESTNEIGHBOR, ZOOM
+from geopyspark.geotrellis.constants import RESAMPLE_METHODS, NEARESTNEIGHBOR, ZOOM
 
 class PngRDD(object):
-    def __init__(self, pyramid, rampname):
+    def __init__(self, pyramid, ramp_name):
+        """Convert a pyramid of TiledRasterRDDs into a displayable structure of PNGs
+
+        Args:
+            pyramid (list): A pyramid of TiledRasterRDD resulting from calling the pyramid 
+                method on an instance of that class
+            ramp_name (str): The name of a color ramp; options are hot, coolwarm, magma, 
+                inferno, plasma, viridis, BlueToOrange, LightYellowToOrange, BlueToRed, 
+                GreenToRedOrange, LightToDarkSunset, LightToDarkGreen, HeatmapYellowToRed, 
+                HeatmapBlueToYellowToRedSpectrum, HeatmapDarkRedToYellowWhite, 
+                HeatmapLightPurpleToDarkPurpleToWhite, ClassificationBoldLandUse, and 
+                ClassificationMutedTerrain
+
+        Returns: A PngRDD object
+        """
         level0 = pyramid[0]
         self.geopysc = level0.geopysc
         self.rdd_type = level0.rdd_type
         self.layer_metadata = list(map(lambda lev: lev.layer_metadata, pyramid))
         self.max_zoom = level0.zoom_level
-        self.pngpyramid = list(map(lambda layer: self.geopysc._jvm.geopyspark.geotrellis.PngRDD.asSingleband(layer.srdd, rampname), pyramid))
+        self.pngpyramid = list(map(lambda layer: self.geopysc._jvm.geopyspark.geotrellis.PngRDD.asSingleband(layer.srdd, ramp_name), pyramid))
 
     @classmethod
-    def makePyramid(cls, tiledrdd, rampname, start_zoom=None, end_zoom=0, resamplemethod=NEARESTNEIGHBOR):
+    def makePyramid(cls, tiledrdd, ramp_name, start_zoom=None, end_zoom=0, resample_method=NEARESTNEIGHBOR):
+        """Create a pyramided PngRDD from a TiledRasterRDD
+
+        Args:
+            tiledrdd (TiledRasterRDD): The TiledRasterRDD source
+            ramp_name (str): The name of a color ramp; options are hot, coolwarm, magma, 
+                inferno, plasma, viridis, BlueToOrange, LightYellowToOrange, BlueToRed, 
+                GreenToRedOrange, LightToDarkSunset, LightToDarkGreen, HeatmapYellowToRed, 
+                HeatmapBlueToYellowToRedSpectrum, HeatmapDarkRedToYellowWhite, 
+                HeatmapLightPurpleToDarkPurpleToWhite, ClassificationBoldLandUse, and 
+                ClassificationMutedTerrain
+            start_zoom (int, optional): The starting (highest resolution) zoom level for 
+                the pyramid.  Defaults to the zoom level of the source RDD.
+            end_zoom (int, optional): The final (lowest resolution) zoom level for the 
+                pyramid.  Defaults to 0.
+            resample_method (str, optional): The resample method to use for the reprojection.
+                This is represented by a constant. If none is specified, then NEARESTNEIGHBOR
+                is used.
+
+        Returns: A PngRDD object
+        """
+        if resample_method not in RESAMPLE_METHODS:
+            raise ValueError(resample_method, " Is not a known resample method.")
+
         reprojected = tiledrdd.reproject("EPSG:3857", scheme=ZOOM)
 
         if not start_zoom:
@@ -19,9 +56,9 @@ class PngRDD(object):
             else:
                 raise AttributeError("No initial zoom level is available; Please provide a value for start_zoom")
 
-        pyramid = reprojected.pyramid(start_zoom, end_zoom, resamplemethod)
+        pyramid = reprojected.pyramid(start_zoom, end_zoom, resample_method)
 
-        return cls(pyramid, rampname)
+        return cls(pyramid, ramp_name)
 
     def lookup(self, col, row, zoom=None):
         """Return the value(s) in the image of a particular SpatialKey (given by col and row)
@@ -30,7 +67,7 @@ class PngRDD(object):
             col (int): The SpatialKey column
             row (int): The SpatialKey row
 
-        Returns: An array of numpy arrays (the tiles)
+        Returns: A list of bytes containing the resulting PNG images
         """
         if not zoom:
             idx = 0
