38fb42c2d4c11a8a6d851865a31f1c767bc7984f
jpolchlo <jpolchlopek@azavea.com>
2017-05-16 12:36:22 -0400

    Updating configuration files
    Signed-off-by: jpolchlo <jpolchlopek@azavea.com>

--- geopyspark-backend/project/Version.scala
+++ geopyspark-backend/project/Version.scala
@@ -1,6 +1,6 @@
 object Version {
   val geopyspark = "0.1.0"
-  val geotrellis = "1.1.0-RC6"
+  val geotrellis = "1.1.0-SNAPSHOT"
   val scala       = "2.11.8"
   val crossScala  = Seq("2.11.8", "2.10.6")
   val scalaTest   = "2.2.0"
