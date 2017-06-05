2d0da0877ea1e754e8c919e180016e5a2dfa3e4d
jpolchlo <jpolchlopek@azavea.com>
2017-06-05 11:11:16 -0400

    Documented hillshade method
    Signed-off-by: jpolchlo <jpolchlopek@azavea.com>

--- geopyspark/geotrellis/rdd.py
+++ geopyspark/geotrellis/rdd.py
@@ -857,6 +857,24 @@ class TiledRasterRDD(CachableRDD):
         return TiledRasterRDD(self.geopysc, self.rdd_type, srdd)
 
     def hillshade(self, band=0, azimuth=315.0, altitude=45.0, z_factor=1.0):
+        """Computes Hillshade (shaded relief) from a raster.
+
+        The resulting raster will be a shaded relief map (a hill shading) based
+        on the sun altitude, azimuth, and the z factor. The z factor is a
+        conversion factor from map units to elevation units.
+
+        Returns a raster of ShortConstantNoDataCellType.
+
+        For descriptions of parameters, please see Esri Desktop's
+        `description <http://goo.gl/DtVDQ>`_ of Hillshade.
+
+        Args:
+            band (int) [default = 0]: The band of the raster to base the
+                hillshade calculation on.
+            azimuth (float) [default = 315]
+            altitude (float) [default = 45]
+            z_factor (float) [default = 1.0]
+        """
         srdd = self.srdd.hillshade(self.geopysc.sc, azimuth, altitude, z_factor, band)
 
         return TiledRasterRDD(self.geopysc, self.rdd_type, srdd)
