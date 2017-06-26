49d59d566c71c2daf8ec96fe2b7d02d2bada55d5
jpolchlo <jpolchlopek@azavea.com>
2017-06-26 15:43:25 -0400

    Adjust tests to use new SparkContext construction method
    Signed-off-by: jpolchlo <jpolchlopek@azavea.com>

--- geopyspark/__init__.py
+++ geopyspark/__init__.py
@@ -3,10 +3,52 @@ ensure_pyspark()
 
 import os
 import glob
-from pyspark import SparkConf
 from pkg_resources import resource_filename
 
 from geopyspark.geopyspark_constants import JAR
+
+from pyspark import RDD, SparkConf, SparkContext
+from pyspark.serializers import AutoBatchedSerializer
+
+
+def map_key_input(key_type, is_boundable):
+    """Gets the mapped GeoTrellis type from the `key_type`.
+    Args:
+        key_type (str): The type of the ``K`` in the tuple, ``(K, V)`` in the RDD.
+        is_boundable (bool): Is ``K`` boundable.
+    Returns:
+        The corresponding GeoTrellis type.
+    """
+
+    if is_boundable:
+        if key_type == "spatial":
+            return "SpatialKey"
+        elif key_type == "spacetime":
+            return "SpaceTimeKey"
+        else:
+            raise Exception("Could not find key type that matches", key_type)
+    else:
+        if key_type == "spatial":
+            return "ProjectedExtent"
+        elif key_type == "spacetime":
+            return "TemporalProjectedExtent"
+        else:
+            raise Exception("Could not find key type that matches", key_type)
+
+def create_python_rdd(pysc, jrdd, serializer):
+    """Creates a Python RDD from a RDD from Scala.
+    Args:
+        jrdd (org.apache.spark.api.java.JavaRDD): The RDD that came from Scala.
+        serializer (:class:`~geopyspark.AvroSerializer` or pyspark.serializers.AutoBatchedSerializer(AvroSerializer)):
+            An instance of ``AvroSerializer`` that is either alone, or wrapped by ``AutoBatchedSerializer``.
+    Returns:
+        ``pyspark.RDD``
+    """
+
+    if isinstance(serializer, AutoBatchedSerializer):
+        return RDD(jrdd, pysc, serializer)
+    else:
+        return RDD(jrdd, pysc, AutoBatchedSerializer(serializer))
 
 def initial_spark_conf(appName=None, master=None):
     conf = SparkConf()
@@ -51,7 +93,7 @@ def initial_spark_conf(appName=None, master=None):
     if len(jars) == 0:
         raise IOError("Failed to find any jars. Looked at these paths {}".format(possible_jars))
 
-    jar_string = str(jars[0])
+    jar_string = ",".join(jars)
     conf.set(key='spark.jars', value=jar_string)
 
     if 'TRAVIS' in os.environ:
--- geopyspark/tests/__init__.py
+++ geopyspark/tests/__init__.py
@@ -1,6 +1,6 @@
 import os
 
 
-if 'JARS' not in os.environ:
-    from geopyspark.geopyspark_utils import setup_environment
-    setup_environment()
+# if 'JARS' not in os.environ:
+#     from geopyspark.geopyspark_utils import setup_environment
+#     setup_environment()
