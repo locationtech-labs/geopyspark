3fa5e24c1d47018c7b8daa0867b32c7cf04d803c
jbouffard <jbouffard@azavea.com>
2017-07-12 13:10:11 -0400

    Updated the GeoTrellis version

--- geopyspark-backend/project/Version.scala
+++ geopyspark-backend/project/Version.scala
@@ -1,6 +1,6 @@
 object Version {
   val geopyspark = "0.1.0"
-  val geotrellis = "1.2.0-M1-SNAPSHOT"
+  val geotrellis = "1.2.0-SNAPSHOT"
   val scala       = "2.11.11"
   val crossScala  = Seq("2.11.11", "2.10.6")
   val scalaTest   = "2.2.0"
