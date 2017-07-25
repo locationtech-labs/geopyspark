b8419864f947bc0064ffa4fd3db3c2a652f1e6b9
jbouffard <jbouffard@azavea.com>
2017-07-25 14:50:17 -0400

    Broke up the tests more so that they are tested in smaller batches

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup_args = dict(
         'geopyspark.command',
         'geopyspark.tests',
         'geopyspark.tests.schema_tests',
+        'geopyspark.tests.tiled_layer_tests',
         'geopyspark.jars',
     ],
     entry_points={
