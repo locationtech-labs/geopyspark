defaf543425c14a8999a951b9198c63c870a908a
jbouffard <jbouffard@azavea.com>
2017-08-25 09:26:54 -0400

    Updated the version numbers to 0.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info < (3, 3):
 
 setup_args = dict(
     name='geopyspark',
-    version='0.2.0',
+    version='0.2.1',
     author='Jacob Bouffard, James McClain',
     author_email='jbouffard@azavea.com, jmcclain@azavea.com',
     download_url='http://github.com/locationtech-labs/geopyspark',
