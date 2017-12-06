a39d3848697fed0d2e9eb40531f579ffbb582338
James W. McClain <jmcclain@azavea.com>
2017-12-05 19:39:52 -0500

    Address Lint

--- geopyspark/rasterio/read.py
+++ geopyspark/rasterio/read.py
@@ -1,8 +1,8 @@
+import os
+import math
 import rasterio
 import geopyspark as gps
 import numpy as np
-import os
-import math
 
 
 # On driver
@@ -32,11 +32,11 @@ def __get_metadata(uri, dataset_to_proj4, xcols, ycols):
         for w in ws:
             ((row_start, row_stop), (col_start, col_stop)) = w
 
-            left  = bounds.left + (bounds.right - bounds.left)*(float(col_start)/width)
+            left = bounds.left + (bounds.right - bounds.left)*(float(col_start)/width)
             right = bounds.left + (bounds.right - bounds.left)*(float(col_stop)/ width)
             bottom = bounds.top + (bounds.bottom - bounds.top)*(float(row_stop)/height)
             top = bounds.top + (bounds.bottom - bounds.top)*(float(row_start)/height)
-            extent = gps.Extent(left,bottom,right,top)
+            extent = gps.Extent(left, bottom, right, top)
 
             new_line = {}
             new_line['uri'] = uri
@@ -52,7 +52,7 @@ def __get_metadata(uri, dataset_to_proj4, xcols, ycols):
             proj4 = dataset_to_proj4(dataset)
             tile_cols = (int)(math.ceil(width/xcols)) * xcols
             tile_rows = (int)(math.ceil(height/ycols)) * ycols
-            ws = [((x, min(width-1,x + xcols)), (y, min(height-1,y + ycols))) for x in range(0, tile_cols, xcols) for y in range(0, tile_rows, ycols)]
+            ws = [((x, min(width-1, x + xcols)), (y, min(height-1, y + ycols))) for x in range(0, tile_cols, xcols) for y in range(0, tile_rows, ycols)]
             metadata = [i for i in windows(ws, uri, proj4)]
     except:
         metadata = []
@@ -77,11 +77,11 @@ def uri_to_pretiles(uri, dataset_to_proj4, xcols=512, ycols=512):
 
 
 def pretile_to_tile(pretile, no_data_value=0):
-    if type(pretile) == type(()):
+    if isinstance(pretile, tuple):
         projected_extent = pretile[0]
         array = np.array([l['data'] for l in pretile[1]])
-    elif type(pretile) == type({}):
-          projected_extent = pretile['projected_extent']
-          array = np.array([pretile['data']])
+    elif isinstance(pretile, dict):
+        projected_extent = pretile['projected_extent']
+        array = np.array([pretile['data']])
     tile = gps.Tile.from_numpy_array(array, no_data_value=no_data_value)
     return (projected_extent, tile)
