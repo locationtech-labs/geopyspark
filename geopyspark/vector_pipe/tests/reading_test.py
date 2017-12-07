1eca3967ce85994ea46939ad9f9ecfc27d9288dc
jbouffard <jbouffard@azavea.com>
2017-12-07 11:40:54 -0500

    Added a reading test

---
+++ geopyspark/vector_pipe/tests/reading_test.py
@@ -0,0 +1,49 @@
+import unittest
+import pytest
+
+from geopyspark.vector_pipe import osm_reader
+from geopyspark.vector_pipe.vector_pipe_constants import LoggingStrategy
+from geopyspark.tests.base_test_class import BaseTestClass
+
+
+# This test should probably be removed before the VP integration
+# branch is merged into master
+class ReadingTest(BaseTestClass):
+
+    @pytest.fixture(autouse=True)
+    def tearDown(self):
+        yield
+        BaseTestClass.pysc._gateway.close()
+
+    def test_reading_snapshot_nothing(self):
+        read = osm_reader.read("/tmp/andorra.orc", logging_strategy='Nothing')
+        points = read.get_point_features_rdd()
+        lines = read.get_line_features_rdd()
+        polys = read.get_polygon_features_rdd()
+        mpolys = read.get_multipolygon_features_rdd()
+
+    def test_reading_snapshot_log4(self):
+        read = osm_reader.read("/tmp/andorra.orc", logging_strategy=LoggingStrategy.LOG4J)
+        points = read.get_point_features_rdd()
+        lines = read.get_line_features_rdd()
+        polys = read.get_polygon_features_rdd()
+        mpolys = read.get_multipolygon_features_rdd()
+
+    def test_reading_snapshot_std(self):
+        read = osm_reader.read("/tmp/andorra.orc", logging_strategy=LoggingStrategy.STD)
+        points = read.get_point_features_rdd()
+        lines = read.get_line_features_rdd()
+        polys = read.get_polygon_features_rdd()
+        mpolys = read.get_multipolygon_features_rdd()
+
+    def test_reading_historical(self):
+        read = osm_reader.read("/tmp/andorra.orc", logging_strategy='Nothing')
+        points = read.get_point_features_rdd()
+        lines = read.get_line_features_rdd()
+        polys = read.get_polygon_features_rdd()
+        mpolys = read.get_multipolygon_features_rdd()
+
+
+
+if __name__ == "__main__":
+    unittest.main()
