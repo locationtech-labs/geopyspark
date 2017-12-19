76c2ffeba4ce38adbb595db320a47d8b14f90c31
jbouffard <jbouffard@azavea.com>
2017-12-19 12:46:00 -0500

    Updated setup.py so that now tests will not be added to the build

--- setup.py
+++ setup.py
@@ -27,13 +27,7 @@ setup_args = dict(
         'geopyspark.vector_pipe',
         'geopyspark.vector_pipe.protobuf',
         'geopyspark.command',
-        'geopyspark.tests',
-        'geopyspark.tests.geotrellis',
-        'geopyspark.tests.geotrellis.schema_tests',
-        'geopyspark.tests.geotrellis.tiled_layer_tests',
-        'geopyspark.tests.geotrellis.io_tests',
-        'geopyspark.tests.vector_pipe',
-        'geopyspark.jars',
+        'geopyspark.jars'
     ],
     entry_points={
         "console_scripts": ['geopyspark = geopyspark.command.configuration:main']
