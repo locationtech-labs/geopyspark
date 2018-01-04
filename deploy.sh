4d17af76c480e08c05a40f5f6b4cc28b5f15ae08
jbouffard <jbouffard@azavea.com>
2018-01-04 12:14:03 -0500

    Fixed the deploy script so that it will now be in the correct directory

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 set -e
 set -x
 
-./sbt "++TRAVIS_SCALA_VERSION" -Dbintray.user=$BINTRAY_USER -Dbintray.pass=$BINTRAY_PASS "project geotrellis-backend" publish
+cd geopyspark-backend && ./sbt "++TRAVIS_SCALA_VERSION" -Dbintray.user=$BINTRAY_USER -Dbintray.pass=$BINTRAY_PASS "project geotrellis-backend" publish
