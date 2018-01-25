7e4e6fed556e98a6c7be47474d7d5f8b839786b0
jbouffard <jbouffard@azavea.com>
2018-01-25 08:13:50 -0500

    Resolved #624 by changing GEOTRELLIS_JAR to JAR

--- geopyspark/geopyspark_constants.py
+++ geopyspark/geopyspark_constants.py
@@ -5,7 +5,7 @@ from os import path
 VERSION = '0.3.0'
 
 """Backend jar name."""
-GEOTRELLIS_JAR = 'geotrellis-backend-assembly-' + VERSION + '.jar'
+JAR = 'geotrellis-backend-assembly-' + VERSION + '.jar'
 
 """The current location of this file."""
 CWD = path.abspath(path.dirname(__file__))
