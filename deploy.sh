289e26df91b6745a7f7b29b7b0229e3903c19c86
Jacob Bouffard <jbouffard@azavea.com>
2018-03-08 14:44:00 -0500

    Travis will now upload the latest backend jar to S3
    Travis will now only remove the most recent jar
    
    Removed the --user install from travis

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 set -e
 set -x
 
-cd geopyspark-backend \
+aws s3 cp geopyspark/jars/geotrellis-backend-assembly-*.jar s3://geopyspark-dependency-jars/ \
+  && cd geopyspark-backend \
   && ./sbt -Dbintray.user=$BINTRAY_USER -Dbintray.pass=$BINTRAY_PASS "project geotrellis-backend" publish \
   && ./sbt -Dbintray.user=$BINTRAY_USER -Dbintray.pass=$BINTRAY_PASS "project vectorpipe" publish \
   && ./sbt -Dbintray.user=$BINTRAY_USER -Dbintray.pass=$BINTRAY_PASS "project geotools" publish
