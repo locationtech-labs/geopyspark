5c969f79356a348c4683aeb8bbd0740ba6902473
Jacob Bouffard <jbouffard@azavea.com>
2018-10-25 14:43:59 -0400

    Implemented readToLayout for SpatialTiledRasterLayer

--- geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/vlm/RasterSource.scala
+++ geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/vlm/RasterSource.scala
@@ -1,6 +1,7 @@
 package geopyspark.geotrellis.vlm
 
-import geopyspark.geotrellis.{PartitionStrategy, TiledRasterLayer}
+import geopyspark.geotrellis.{PartitionStrategy, SpatialTiledRasterLayer}
+import geopyspark.geotrellis.Constants.{GEOTRELLIS, GDAL}
 
 import geotrellis.contrib.vlm._
 import geotrellis.contrib.vlm.gdal.GDALRasterSource
@@ -20,43 +21,83 @@ import scala.collection.JavaConverters._
 
 
 object RasterSource {
-  def tileToLayout(
+  def readToLayout(
     sc: SparkContext,
     layerType: String,
     paths: java.util.ArrayList[String],
-    layout: LayoutScheme,
+    layoutType: LayoutType,
     targetCRS: Option[String],
     resampleMethod: ResampleMethod,
     partitionStrategy: Option[PartitionStrategy],
     readMethod: String
-  ): TiledRasterLayer[_] =
-    tileToLayout(
+  ): SpatialTiledRasterLayer =
+    readToLayout(
       sc,
       layerType,
       sc.parallelize(paths.asScala, paths.size),
-      layout,
+      layoutType,
       targetCRS,
       resampleMethod,
       partitionStrategy,
       readMethod
     )
 
-  def tileToLayout(
+  def readToLayout(
     sc: SparkContext,
     layerType: String,
     rdd: RDD[String],
-    layout: LayoutScheme,
+    layoutType: LayoutType,
     targetCRS: Option[String],
     resampleMethod: ResampleMethod,
     partitionStrategy: Option[PartitionStrategy],
     readMethod: String
-  ): TiledRasterLayer[_] = {
+  ): SpatialTiledRasterLayer = {
     val rasterSourceRDD: RDD[RasterSource] =
-      readMethod match {
-        case "GeoTrellis" => rdd.map { new GeoTiffRasterSource(_) }
-        case "GDAL" => rdd.map { GDALRasterSource }
+      (readMethod match {
+        case GEOTRELLIS => rdd.map { new GeoTiffRasterSource(_): RasterSource }
+        case GDAL => rdd.map { GDALRasterSource(_): RasterSource }
+      }).cache()
+
+    val reprojectedSourcesRDD: RDD[RasterSource] =
+      targetCRS match {
+        case Some(crs) =>
+          rasterSourceRDD.map { _.reproject(CRS.fromString(crs), resampleMethod) }.cache()
+        case None =>
+          rasterSourceRDD
       }
 
-    ???
+    val metadata: RasterSummary = RasterSummary.fromRDD(reprojectedSourcesRDD)
+
+    val layoutScheme: LayoutScheme =
+      layoutType match {
+        case global: GlobalLayout => ZoomedLayoutScheme(metadata.crs, global.tileSize)
+        case local: LocalLayout => FloatingLayoutScheme(local.tileCols, local.tileRows)
+      }
+
+    val layout: LayoutDefinition = metadata.levelFor(layoutScheme).layout
+    val layoutRDD: RDD[LayoutTileSource] = reprojectedSourcesRDD.map { _.tileToLayout(layout, resampleMethod) }
+
+    val collectedMetadata: RasterSummary = RasterSummary.fromRDD(layoutRDD.map { _.source })
+
+    val (tileLayerMetadata, zoom): (TileLayerMetadata[SpatialKey], Option[Int]) =
+      collectedMetadata.toTileLayerMetadata(layoutType)
+
+    val tiledRDD: RDD[(SpatialKey, MultibandTile)] =
+      layoutRDD.flatMap { case source =>
+        source.keys.toIterator.flatMap { key: SpatialKey =>
+          source.rasterRef(key).raster match {
+            case Some(raster) => Some((key, raster.tile))
+            case None => None
+          }
+        }
+      }
+
+    reprojectedSourcesRDD.unpersist()
+    cachedSourcesRDD.unpersist()
+
+    val contextRDD: MultibandTileLayerRDD[SpatialKey] =
+      ContextRDD(tiledRDD, tileLayerMetadata)
+
+    SpatialTiledRasterLayer(zoom, contextRDD)
   }
 }
