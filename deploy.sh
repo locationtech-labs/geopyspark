3a47f3a46d5574cc2fbea1cc84c446b74de4c8d1
Jacob Bouffard <jbouffard@azavea.com>
2018-11-29 14:28:37 -0500

    Removed vectorpipe from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,4 @@ set -x
 aws s3 cp geopyspark/jars/geotrellis-backend-assembly-*.jar s3://geopyspark-dependency-jars/ --acl public-read \
   && cd geopyspark-backend \
   && ./sbt -Dbintray.user=$BINTRAY_USER -Dbintray.pass=$BINTRAY_PASS "project geotrellis-backend" publish \
-  && ./sbt -Dbintray.user=$BINTRAY_USER -Dbintray.pass=$BINTRAY_PASS "project vectorpipe" publish \
   && ./sbt -Dbintray.user=$BINTRAY_USER -Dbintray.pass=$BINTRAY_PASS "project geotools" publish
