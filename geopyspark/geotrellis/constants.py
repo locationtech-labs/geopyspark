d0b6a92929ebdac81c203f58fae5d4b3e042454f
Jacob Bouffard <jbouffard@azavea.com>
2018-12-05 10:39:13 -0500

    Added the ReadMethod class to __all__

--- geopyspark/geotrellis/constants.py
+++ geopyspark/geotrellis/constants.py
@@ -6,7 +6,7 @@ __all__ = ['NO_DATA_INT', 'LayerType', 'IndexingMethod', 'ResampleMethod', 'Time
            'Operation', 'Neighborhood', 'ClassificationStrategy', 'CellType', 'ColorRamp',
            'DEFAULT_MAX_TILE_SIZE', 'DEFAULT_PARTITION_BYTES', 'DEFAULT_CHUNK_SIZE',
            'DEFAULT_GEOTIFF_TIME_TAG', 'DEFAULT_GEOTIFF_TIME_FORMAT', 'DEFAULT_S3_CLIENT',
-           'StorageMethod', 'ColorSpace', 'Compression', 'Unit']
+           'StorageMethod', 'ColorSpace', 'Compression', 'Unit', 'ReadMethod']
 
 
 """The NoData value for ints in GeoTrellis."""
