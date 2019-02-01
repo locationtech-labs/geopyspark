9531888e320ff71966f0c3f9ff5c281b4e46fddb
Jacob Bouffard <jbouffard@azavea.com>
2019-02-01 10:52:25 -0500

    Removed the readOrdered method

--- geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/vlm/RasterSource.scala
+++ geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/vlm/RasterSource.scala
@@ -21,7 +21,6 @@ import org.apache.spark.{SparkContext, Partitioner}
 import org.apache.spark.rdd.RDD
 
 import scala.collection.JavaConverters._
-import scala.collection.mutable.ArrayBuffer
 
 
 object RasterSource {
@@ -85,110 +84,6 @@ object RasterSource {
     rasterSourceRDD.unpersist()
 
     ProjectedRasterLayer(projectedRasterRDD)
-  }
-
-  def readOrdered(
-    sc: SparkContext,
-    layerType: String,
-    paths: java.util.ArrayList[String],
-    maxTileSize: Int,
-    targetCRS: String,
-    numPartitions: Integer,
-    resampleMethod: ResampleMethod,
-    readMethod: String
-  ): ProjectedRasterLayer = {
-    val scalaPaths: Seq[Seq[(String, String)]] = ??? ///paths.asScala.toSeq.map { ReadingInfo( }
-
-    val partitions =
-      numPartitions match {
-        case i: Integer => Some(i.toInt)
-        case null => None
-      }
-
-    readOrdered(
-      sc,
-      layerType,
-      sc.parallelize(scalaPaths, partitions.getOrElse(scalaPaths.size)),
-      targetCRS,
-      partitions,
-      resampleMethod,
-      readMethod
-    )
-  }
-
-  def readOrdered(
-    sc: SparkContext,
-    layerType: String,
-    rdd: RDD[Seq[(String, String)]],
-    targetCRS: String,
-    numPartitions: Option[Int],
-    resampleMethod: ResampleMethod,
-    readMethod: String
-  ): ProjectedRasterLayer = {
-    val rasterSourcesRDD: RDD[Seq[(String, RasterSource)]] =
-      (readMethod match {
-        case GEOTRELLIS =>
-          rdd.mapPartitions { iter =>
-            iter.map { files: Seq[(String, String)] =>
-              files.map { case (k, v) => (k, new GeoTiffRasterSource(v): RasterSource)
-              }
-            }
-          }
-        case GDAL =>
-          rdd.mapPartitions { iter =>
-            iter.map { files: Seq[(String, String)] =>
-              files.map { case (k, v) => (k, GDALRasterSource(v): RasterSource)
-              }
-            }
-          }
-      }).cache()
-
-    val reprojectedSourcesRDD: RDD[(String, RasterSource)] =
-      targetCRS match {
-        case crs: String =>
-          rasterSourcesRDD.flatMap { case sources =>
-            sources.map { case (index, source) =>
-              (index, source.reproject(CRS.fromString(crs), resampleMethod))
-            }
-          }
-        case null => rasterSourcesRDD.flatMap { sources => sources }
-      }
-
-    val rasterSummary: RasterSummary =
-      reprojectedSourcesRDD
-        .map { case (_, source) =>
-            val ProjectedExtent(extent, crs) = source.getComponent[ProjectedExtent]
-            val cellSize = CellSize(extent, source.cols, source.rows)
-            RasterSummary(crs, source.cellType, cellSize, extent, source.size, 1)
-          }.reduce { _ combine _ }
-
-    val keyedSourcesRDD: RDD[(Extent, (String, RasterSource))] =
-      reprojectedSourcesRDD.map { case (index, source) =>
-        (source.extent, (index, source))
-      }
-
-    val groupedSourcesRDD: RDD[(Extent, Iterable[(String, RasterSource)])] =
-      keyedSourcesRDD.groupByKey(numPartitions.getOrElse(rasterSummary.estimatePartitionsNumber))
-
-    val projectedRDD: RDD[(ProjectedExtent, MultibandTile)] =
-      groupedSourcesRDD.map { case (ex, iter) =>
-        val sorted = iter.toSeq.sortBy { _._1 }
-        val crs = sorted.head._2.crs
-
-        val tiles: Seq[MultibandTile] =
-          sorted.flatMap { case (_, source) =>
-            source.read(source.extent) match {
-              case Some(raster) => Some(raster.tile)
-              case None => None
-            }
-          }
-
-        (ProjectedExtent(ex, crs), MultibandTile(tiles.map { _.band(0) }))
-      }
-
-    rasterSourcesRDD.unpersist()
-
-    ProjectedRasterLayer(projectedRDD)
   }
 
   def readToLayout(
