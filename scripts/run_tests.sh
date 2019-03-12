db3bb158f170ffba421ea754366c2f409cf94bcd
Eugene Cheipesh <echeipesh@gmail.com>
2019-03-12 09:44:19 -0400

    Test with spark 2.3.3

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,12 +1,12 @@
 apt-get update -y && apt-get install -y wget python3-pip awscli && \
 pushd geopyspark-backend && ./sbt "project geotrellis-backend" assembly && \
 cp geotrellis/target/scala-2.11/geotrellis-backend-assembly-*.jar ../geopyspark/jars && popd && \
-if [ ! -f archives/spark-2.4.0-bin-hadoop2.7.tgz ]; then
+if [ ! -f archives/spark-2.3.3-bin-hadoop2.7.tgz ]; then
   cd archives
-  wget http://apache.cs.utah.edu/spark/spark-2.4.0/spark-2.4.0-bin-hadoop2.7.tgz
+  wget http://apache.cs.utah.edu/spark/spark-2.3.3/spark-2.3.3-bin-hadoop2.7.tgz
   cd ~-
 fi && \
-tar -xvf archives/spark-2.4.0-bin-hadoop2.7.tgz && \
+tar -xvf archives/spark-2.3.3-bin-hadoop2.7.tgz && \
 pip3 install -r requirements.txt && \
 pip3 install pyproj && \
 pip3 install matplotlib==2.0.0 && \
@@ -15,7 +15,7 @@ pip3 install colortools==0.1.2 && \
 pip3 install awscli && \
 export PATH=$PATH:$HOME/.local/bin && \
 pip3 install . && \
-export SPARK_HOME=./spark-2.4.0-bin-hadoop2.7/ && \
+export SPARK_HOME=./spark-2.3.3-bin-hadoop2.7/ && \
 ./scripts/test_script.sh && \
 if [[ $TRAVIS_PYTHON_VERSION != "3.3" ]]; then
   pylint geopyspark;
